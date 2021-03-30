d664359f58214de47a7c5bfa2908095bbeca2878
filterpaper <filterpaper@localhost>
2021-03-30 19:04:06 +0800

    Update consistency of PROGMEN declaration.

--- mod-status.c
+++ mod-status.c
@@ -24,11 +24,11 @@
 #include "filterpaper.h"
 
 static void render_logo(void) {
-	static char const PROGMEM corne_logo[] = {
+	static char const corne_logo[] PROGMEM = {
 		0x80, 0x81, 0x82, 0x83, 0x84,
 		0xa0, 0xa1, 0xa2, 0xa3, 0xa4,
 		0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0};
-	static char const PROGMEM katakana[] = {
+	static char const katakana[] PROGMEM = {
 		0x20, 0xd1, 0xd2, 0xd3, 0x20, 0};
 
 	oled_write_P(corne_logo, false);
@@ -38,19 +38,19 @@ static void render_logo(void) {
 
 // Graphical layer display
 static void render_layer_state(void) {
-	static char const PROGMEM default_layer[] = {
+	static char const default_layer[] PROGMEM = {
 		0x20, 0x94, 0x95, 0x96, 0x20,
 		0x20, 0xb4, 0xb5, 0xb6, 0x20,
 		0x20, 0xd4, 0xd5, 0xd6, 0x20, 0};
-	static char const PROGMEM raise_layer[] = {
+	static char const raise_layer[] PROGMEM = {
 		0x20, 0x97, 0x98, 0x99, 0x20,
 		0x20, 0xb7, 0xb8, 0xb9, 0x20,
 		0x20, 0xd7, 0xd8, 0xd9, 0x20, 0};
-	static char const PROGMEM lower_layer[] = {
+	static char const lower_layer[] PROGMEM = {
 		0x20, 0x9a, 0x9b, 0x9c, 0x20,
 		0x20, 0xba, 0xbb, 0xbc, 0x20,
 		0x20, 0xda, 0xdb, 0xdc, 0x20, 0};
-	static char const PROGMEM adjust_layer[] = {
+	static char const adjust_layer[] PROGMEM = {
 		0x20, 0x9d, 0x9e, 0x9f, 0x20,
 		0x20, 0xbd, 0xbe, 0xbf, 0x20,
 		0x20, 0xdd, 0xde, 0xdf, 0x20, 0};
@@ -62,25 +62,25 @@ static void render_layer_state(void) {
 }
 
 static void render_mod_status_gui_alt(uint_fast8_t const modifiers) {
-	static char const PROGMEM gui_off_1[] = {0x85, 0x86, 0};
-	static char const PROGMEM gui_off_2[] = {0xa5, 0xa6, 0};
-	static char const PROGMEM gui_on_1[] = {0x8d, 0x8e, 0};
-	static char const PROGMEM gui_on_2[] = {0xad, 0xae, 0};
+	static char const gui_off_1[] PROGMEM = {0x85, 0x86, 0};
+	static char const gui_off_2[] PROGMEM = {0xa5, 0xa6, 0};
+	static char const gui_on_1[] PROGMEM = {0x8d, 0x8e, 0};
+	static char const gui_on_2[] PROGMEM = {0xad, 0xae, 0};
 
-	static char const PROGMEM alt_off_1[] = {0x87, 0x88, 0};
-	static char const PROGMEM alt_off_2[] = {0xa7, 0xa8, 0};
-	static char const PROGMEM alt_on_1[] = {0x8f, 0x90, 0};
-	static char const PROGMEM alt_on_2[] = {0xaf, 0xb0, 0};
+	static char const alt_off_1[] PROGMEM = {0x87, 0x88, 0};
+	static char const alt_off_2[] PROGMEM = {0xa7, 0xa8, 0};
+	static char const alt_on_1[] PROGMEM = {0x8f, 0x90, 0};
+	static char const alt_on_2[] PROGMEM = {0xaf, 0xb0, 0};
 
 	// fillers between the modifier icons bleed into the icon frames
-	static char const PROGMEM off_off_1[] = {0xc5, 0};
-	static char const PROGMEM off_off_2[] = {0xc6, 0};
-	static char const PROGMEM on_off_1[] = {0xc7, 0};
-	static char const PROGMEM on_off_2[] = {0xc8, 0};
-	static char const PROGMEM off_on_1[] = {0xc9, 0};
-	static char const PROGMEM off_on_2[] = {0xca, 0};
-	static char const PROGMEM on_on_1[] = {0xcb, 0};
-	static char const PROGMEM on_on_2[] = {0xcc, 0};
+	static char const off_off_1[] PROGMEM = {0xc5, 0};
+	static char const off_off_2[] PROGMEM = {0xc6, 0};
+	static char const on_off_1[] PROGMEM = {0xc7, 0};
+	static char const on_off_2[] PROGMEM = {0xc8, 0};
+	static char const off_on_1[] PROGMEM = {0xc9, 0};
+	static char const off_on_2[] PROGMEM = {0xca, 0};
+	static char const on_on_1[] PROGMEM = {0xcb, 0};
+	static char const on_on_2[] PROGMEM = {0xcc, 0};
 
 	if (modifiers & MOD_MASK_GUI) { oled_write_P(gui_on_1, false); }
 	else { oled_write_P(gui_off_1, false); }
@@ -106,25 +106,25 @@ static void render_mod_status_gui_alt(uint_fast8_t const modifiers) {
 }
 
 static void render_mod_status_ctrl_shift(uint_fast8_t const modifiers, bool const caps) {
-	static char const PROGMEM ctrl_off_1[] = {0x89, 0x8a, 0};
-	static char const PROGMEM ctrl_off_2[] = {0xa9, 0xaa, 0};
-	static char const PROGMEM ctrl_on_1[] = {0x91, 0x92, 0};
-	static char const PROGMEM ctrl_on_2[] = {0xb1, 0xb2, 0};
+	static char const ctrl_off_1[] PROGMEM = {0x89, 0x8a, 0};
+	static char const ctrl_off_2[] PROGMEM = {0xa9, 0xaa, 0};
+	static char const ctrl_on_1[] PROGMEM = {0x91, 0x92, 0};
+	static char const ctrl_on_2[] PROGMEM = {0xb1, 0xb2, 0};
 
-	static char const PROGMEM shift_off_1[] = {0x8b, 0x8c, 0};
-	static char const PROGMEM shift_off_2[] = {0xab, 0xac, 0};
-	static char const PROGMEM shift_on_1[] = {0xcd, 0xce, 0};
-	static char const PROGMEM shift_on_2[] = {0xcf, 0xd0, 0};
+	static char const shift_off_1[] PROGMEM = {0x8b, 0x8c, 0};
+	static char const shift_off_2[] PROGMEM = {0xab, 0xac, 0};
+	static char const shift_on_1[] PROGMEM = {0xcd, 0xce, 0};
+	static char const shift_on_2[] PROGMEM = {0xcf, 0xd0, 0};
 
 	// fillers between the modifier icons bleed into the icon frames
-	static char const PROGMEM off_off_1[] = {0xc5, 0};
-	static char const PROGMEM off_off_2[] = {0xc6, 0};
-	static char const PROGMEM on_off_1[] = {0xc7, 0};
-	static char const PROGMEM on_off_2[] = {0xc8, 0};
-	static char const PROGMEM off_on_1[] = {0xc9, 0};
-	static char const PROGMEM off_on_2[] = {0xca, 0};
-	static char const PROGMEM on_on_1[] = {0xcb, 0};
-	static char const PROGMEM on_on_2[] = {0xcc, 0};
+	static char const off_off_1[] PROGMEM = {0xc5, 0};
+	static char const off_off_2[] PROGMEM = {0xc6, 0};
+	static char const on_off_1[] PROGMEM = {0xc7, 0};
+	static char const on_off_2[] PROGMEM = {0xc8, 0};
+	static char const off_on_1[] PROGMEM = {0xc9, 0};
+	static char const off_on_2[] PROGMEM = {0xca, 0};
+	static char const on_on_1[] PROGMEM = {0xcb, 0};
+	static char const on_on_2[] PROGMEM = {0xcc, 0};
 
 	if (modifiers & MOD_MASK_CTRL) { oled_write_P(ctrl_on_1, false); }
 	else { oled_write_P(ctrl_off_1, false); }
