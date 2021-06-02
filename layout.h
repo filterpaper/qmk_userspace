e1333186e02e523af62dd0c0bedd2ca3349191c2
filterpaper <filterpaper@localhost>
2021-06-02 18:38:13 +0800

    Brackets next to number pad.

--- layout.h
+++ layout.h
@@ -81,21 +81,21 @@
 	_______, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    _______, KC_N,    KC_E,    KC_I ,   KC_O,    _______, \
 	_______, _______, _______, _______, _______, _______, KC_K,    _______, _______, _______, _______, _______
 #define _NUMB \
-	_______, _______, KC_1,    KC_2,    KC_3,    _______, _______, KC_UNDS, KC_PLUS, KC_PIPE, _______, _______, \
-	_______, _______, KC_4,    KC_5,    KC_6,    _______, _______, KC_MINS, KC_EQL,  KC_BSLS, _______, _______, \
-	KC_CAPS, _______, KC_7,    KC_8,    KC_9,    KC_0,    _______, _______, _______, _______, _______, _______
+	KC_LPRN, KC_RPRN, KC_1,    KC_2,    KC_3,    _______, _______, KC_UNDS, KC_PLUS, KC_PIPE, _______, _______, \
+	KC_LCBR, KC_RCBR, KC_4,    KC_5,    KC_6,    _______, _______, KC_MINS, KC_EQL,  KC_BSLS, _______, _______, \
+	KC_LBRC, KC_RBRC, KC_7,    KC_8,    KC_9,    KC_0,    _______, _______, _______, _______, _______, _______
 #define _NAVI \
-	KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, _______, \
-	KC_GRV,  _______, KC_LPRN, KC_LCBR, KC_RCBR, KC_RPRN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_INS,  KC_VOLU, \
-	_______, _______, _______, KC_LBRC, KC_RBRC, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_DEL,  KC_VOLD
+	KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, _______, _______, _______, _______, \
+	KC_GRV,  _______, KC_CIRC, KC_AMPR, KC_ASTR, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_INS,  KC_VOLU, \
+	KC_CAPS, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_DEL,  KC_VOLD
 #define _FUNC \
-	RESET,   _______, _______, _______, _______, _______, _______, KC_WH_U, KC_WH_D, _______, _______, TG(COLE),\
-	KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, \
-	KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_BTN2, KC_BTN1, _______, _______, _______
+	RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, KC_WH_U, KC_WH_D, _______, _______, TG(COLE),\
+	_______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, \
+	_______, _______, _______, _______, _______, _______, _______, KC_BTN2, KC_BTN1, _______, _______, _______
 
 // Bottom row
 #define C_QWER                                          KC_DEL, LT(NAVI,KC_BSPC), RSFT_T(KC_ENT), RSFT_T(KC_SPC), LT(NUMB,KC_SPC), KC_ESC
-#define P_QWER KC_DEL, ALT_T(KC_VOLD), CTL_T(KC_VOLU), KC_LGUI, LT(NAVI,KC_BSPC), RSFT_T(KC_SPC), RSFT_T(KC_SPC), LT(NUMB,KC_SPC), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+#define P_QWER KC_DEL, ALT_T(KC_VOLD), CTL_T(KC_VOLU), KC_LGUI, LT(NAVI,KC_BSPC), RSFT_T(KC_ENT), RSFT_T(KC_SPC), LT(NUMB,KC_SPC), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
 #define B_QWER KC_DEL, ALT_T(KC_VOLD), CTL_T(KC_VOLU), KC_LGUI, LT(NAVI,KC_BSPC),         RSFT_T(KC_SPC),         LT(NUMB,KC_SPC), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
 
 #define C_NUMB                            _______, MO(FUNC), _______, _______, _______, _______
@@ -169,7 +169,7 @@
 	KC_LBRC, KC_RBRC, KC_7,    KC_8,    KC_9,    KC_EQL,  _______, _______, _______, _______, _______, _______
 #define _SYML \
 	KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, _______, _______, _______, _______, \
-	KC_GRV,  _______, KC_ASTR, KC_AMPR, KC_CIRC, _______, _______, KC_RGUI, KC_RCTL, KC_RALT, KC_RSFT, _______, \
+	KC_GRV,  _______, KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, KC_RGUI, KC_RCTL, KC_RALT, KC_RSFT, _______, \
 	KC_CAPS, _______, KC_BSLS, KC_PIPE, KC_UNDS, _______, _______, _______, _______, _______, _______, _______
 #define _NAVR \
 	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
