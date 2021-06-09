fcbf34fdb93967ccbd353b5c8a3d63c4dae24f8f
filterpaper <filterpaper@localhost>
2021-06-09 19:36:45 +0800

    Update configurator json layout.

--- layout.h
+++ layout.h
@@ -144,10 +144,10 @@
 // Boardsource The Mark 65
 #define MARK_QWERTY \
 	KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_VOLU, \
-	KC_TAB,           Q_TH,    W_TH,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, KC_VOLD, \
+	KC_TAB,           Q_TH,    W_TH,    KC_E,    KC_R,    KC_T,    Y_TH,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, KC_VOLD, \
 	KC_CAPS,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,           KC_MUTE, \
 	OSM_SFT, _______, Z_TH,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    M_TH,    COMM_TH, DOT_TH,  KC_SLSH, KC_RSFT,          KC_UP,            \
-	OSM_ALT, OSM_CTL,          OSM_GUI, LT(2,KC_SPC),     KC_BSPC,          RSFT_T(KC_SPC),   KC_RCTL, KC_RALT, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+	OSM_ALT, OSM_CTL,          OSM_GUI, LT(2,KC_BSPC),    KC_ENT,           RSFT_T(KC_SPC),   KC_RCTL, KC_RALT, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
 #define MARK_COLEMAK \
 	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
 	_______,          _______, _______, KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, _______, _______, _______, _______, \
@@ -157,9 +157,9 @@
 #define MARK_LAYER2 \
 	_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_HOME, KC_PGUP, \
 	_______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,  KC_PGDN, \
-	_______,          _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,          KC_END,  \
-	_______, _______, _______, _______, _______, Z_PASTE, _______, _______, _______, _______, _______, _______, _______,          _______,          \
-	_______, _______,          _______, _______,          _______,          MO(3),            _______, _______, _______, _______, _______, _______
+	_______,          _______, _______, KC_BTN2, KC_BTN1, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,          KC_END,  \
+	_______, _______, _______, _______, _______, Z_PASTE, _______, _______, _______, _______, _______, _______, _______,          KC_MS_U,          \
+	_______, _______,          _______, _______,          _______,          MO(3),            _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R
 #define MARK_LAYER3 \
 	RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, TG(1),   _______, \
 	_______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
