26bb3b5281a5188ebdc7d468a990947015705b9c
filterpaper <filterpaper@localhost>
2021-07-12 10:43:16 +0800

    ACTN macro is more descriptive.

--- combos.h
+++ combos.h
@@ -15,29 +15,34 @@
  */
 
 /* Simplified combo helper file from Jane Bernhardt (http://gboards.ca/)
-   that generates combo source codes at preprocessor stage with
+   that generates combo source codes with preprocessor from
    "combos.def" data file. See: http://combos.gboards.ca/docs/combos/
+
+   Data file format:
+   COMB(name, keycode_shortcut, combo_sequence...)
+   SUBS(name, "string to send", combo_sequence...)
+   ACTN(name, actionfunction(), combo_sequence...)
 
    Merge early_combo patch to work with mod tap:
    git pull https://github.com/sevanteri/qmk_firmware.git early_combo
-*/
+ */
 
 // Combo code macros
-#define C_ENUM(name, val, ...) combo_##name,
-#define C_DATA(name, val, ...) uint16_t const cmb_##name[] PROGMEM = {__VA_ARGS__, COMBO_END};
-#define C_COMB(name, val, ...) [combo_##name] = COMBO(cmb_##name, val),
-#define A_COMB(name, val, ...) [combo_##name] = COMBO_ACTION(cmb_##name),
-#define P_SUBS(name, val, ...) case combo_##name: if (pressed) SEND_STRING(val); break;
-#define P_ACTI(name, val, ...) case combo_##name: if (pressed) val; break;
+#define C_ENUM(name, val, ...) combo_ ## name,
+#define C_DATA(name, val, ...) uint16_t const cmb_ ## name[] PROGMEM = {__VA_ARGS__, COMBO_END};
+#define C_COMB(name, val, ...) [combo_ ## name] = COMBO(cmb_ ## name, val),
+#define A_COMB(name, val, ...) [combo_ ## name] = COMBO_ACTION(cmb_ ## name),
+#define P_SUBS(name, val, ...) case combo_ ## name: if (pressed) SEND_STRING(val); break;
+#define P_ACTN(name, val, ...) case combo_ ## name: if (pressed) val; break;
 #define BLANK(...)
 
 // Enumerate name list
 #undef COMB
 #undef SUBS
-#undef ACTI
+#undef ACTN
 #define COMB C_ENUM
 #define SUBS C_ENUM
-#define ACTI C_ENUM
+#define ACTN C_ENUM
 enum combos {
 	#include "combos.def"
 	COMBO_LENGTH
@@ -47,19 +52,19 @@ uint16_t COMBO_LEN = COMBO_LENGTH;
 // Store combos in PROGMEM
 #undef COMB
 #undef SUBS
-#undef ACTI
+#undef ACTN
 #define COMB C_DATA
 #define SUBS C_DATA
-#define ACTI C_DATA
+#define ACTN C_DATA
 #include "combos.def"
 
 // Fill key array
 #undef COMB
 #undef SUBS
-#undef ACTI
+#undef ACTN
 #define COMB C_COMB
 #define SUBS A_COMB
-#define ACTI A_COMB
+#define ACTN A_COMB
 combo_t key_combos[] = {
 	#include "combos.def"
 };
@@ -67,10 +72,10 @@ combo_t key_combos[] = {
 // Fill event function
 #undef COMB
 #undef SUBS
-#undef ACTI
+#undef ACTN
 #define COMB BLANK
 #define SUBS P_SUBS
-#define ACTI P_ACTI
+#define ACTN P_ACTN
 void process_combo_event(uint16_t combo_index, bool pressed) {
 	switch (combo_index) {
 		#include "combos.def"
