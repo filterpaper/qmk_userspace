b2f6d7b3f0d2b3247745843f636bf80802f65f99
filterpaper <filterpaper@localhost>
2021-07-30 06:59:54 +0800

    Pinkie keys to top row, remove bilateral combos that failed on chords.

--- layout.h
+++ layout.h
@@ -76,14 +76,14 @@
 	_______, _______, _______, _______, _______, _______,     KC_K,    _______, _______, _______, _______, _______, \
 	                           _______, _______, _______,     _______, _______, _______
 #define _NUMB \
-	_______, _______, KC_1,    KC_2,    KC_3,    _______,     _______, KC_LPRN, KC_RPRN, _______, _______, _______, \
+	_______, _______, KC_1,    KC_2,    KC_3,    _______,     _______, KC_LPRN, KC_RPRN, _______, KC_DQUO, _______, \
 	_______, _______, KC_4,    KC_5,    KC_6,    _______,     _______, KC_LCBR, KC_RCBR, _______, KC_QUOT, _______, \
-	_______, _______, KC_7,    KC_8,    KC_9,    KC_0,        _______, KC_LBRC, KC_RBRC, _______, KC_DQUO, _______, \
+	_______, _______, KC_7,    KC_8,    KC_9,    KC_0,        _______, KC_LBRC, KC_RBRC, _______, _______, _______, \
 	                           _______, MO(FNC), _______,     _______, _______, _______
 #define _NAVI \
-	_______, _______, KC_CIRC, KC_AMPR, KC_ASTR, _______,     KC_PGUP, KC_MINS, KC_EQL,  KC_BSLS, _______, _______, \
+	_______, _______, KC_CIRC, KC_AMPR, KC_ASTR, _______,     KC_PGUP, KC_MINS, KC_EQL,  KC_BSLS, KC_TILD, _______, \
 	_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_GRV,  KC_VOLU, \
-	KC_CAPS, _______, _______, _______, _______, _______,     KC_PGDN, KC_UNDS, KC_PLUS, KC_PIPE, KC_TILD, KC_VOLD, \
+	KC_CAPS, _______, _______, _______, _______, _______,     KC_PGDN, KC_UNDS, KC_PLUS, KC_PIPE, _______, KC_VOLD, \
 	                           _______, _______, _______,     _______, MO(FNC), _______
 #define _FUNC \
 	RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_HOME, KC_WH_U, KC_WH_D, KC_INS,  _______, TG(CMK), \
@@ -120,19 +120,19 @@
 	_______, _______, _______, _______, _______,     KC_K,    _______, _______, _______, _______, \
 	                  _______, _______, _______,     _______, _______, _______
 #define W_NUMB \
-	_______, KC_1,    KC_2,    KC_3,    _______,     _______, KC_LPRN, KC_RPRN, _______, _______, \
+	_______, KC_1,    KC_2,    KC_3,    _______,     _______, KC_LPRN, KC_RPRN, _______, KC_DQUO, \
 	KC_VOLU, KC_4,    KC_5,    KC_6,    _______,     _______, KC_LCBR, KC_RCBR, _______, KC_QUOT, \
-	KC_VOLD, KC_7,    KC_8,    KC_9,    KC_0,        _______, KC_LBRC, KC_RBRC, _______, KC_DQUO, \
+	KC_VOLD, KC_7,    KC_8,    KC_9,    KC_0,        _______, KC_LBRC, KC_RBRC, _______, _______, \
 	                 _______, MO(FNC), _______,      _______, _______, _______
 #define W_NAVI \
-	_______, KC_CIRC, KC_AMPR, KC_ASTR, _______,     KC_PGUP, KC_MINS, KC_EQL,  KC_BSLS, _______, \
+	_______, KC_CIRC, KC_AMPR, KC_ASTR, _______,     KC_PGUP, KC_MINS, KC_EQL,  KC_BSLS, KC_TILD, \
 	KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_GRV,  \
-	KC_CAPS, _______, _______, _______, _______,     KC_PGDN, KC_UNDS, KC_PLUS, KC_PIPE, KC_TILD, \
+	KC_CAPS, _______, _______, _______, _______,     KC_PGDN, KC_UNDS, KC_PLUS, KC_PIPE, _______, \
 		              _______, _______, _______,     _______, MO(FNC), _______
 #define W_FUNC \
-	KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_HOME, KC_WH_U, KC_WH_D, KC_INS,  _______, \
+	KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_HOME, KC_WH_U, KC_WH_D, KC_INS,  TG(CMK), \
 	KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, \
-	RESET,   _______, _______, Z_PASTE, _______,     KC_END,  KC_BTN2, KC_BTN1, KC_DEL,  TG(CMK), \
+	Z_SLEEP, _______, _______, Z_PASTE, _______,     KC_END,  KC_BTN2, KC_BTN1, KC_DEL,  RESET,   \
 	                  _______, _______, _______,     _______, _______, _______
 
 
