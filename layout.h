9b8149d92eb55fd0952fc06052c7bfd066208d2d
filterpaper <filterpaper@localhost>
2021-08-04 18:41:20 +0800

    Horizontal brackets and wrapper update for 3w6.

--- layout.h
+++ layout.h
@@ -40,11 +40,16 @@
 #define OSS_LT2	LT(2,KC_NO) // OSM shift layer tap
 #define OSS_LT() if (record->tap.count) { if (record->event.pressed) add_oneshot_mods(MOD_LSFT); return false }
 
+#define BSE 0
+#define CMK 1
+#define NUM 2
+#define NAV 3
+#define FNC 4
+
 
 
 // Corne 3x6_3
 #define CORNE_wrapper(...)  LAYOUT_split_3x6_3(__VA_ARGS__)
-
 // Home row mod wrapper
 #define HRM(k) HRM_SACG(k)
 #define HRM_SACG( \
@@ -59,32 +64,26 @@
 	k25, k26, k27, k28, k29, k30, k31, k32, k33, k34, k35, k36, \
 	k37, k38, k39, k40, k41, k42
 
-#define BSE 0
-#define CMK 1
-#define NUM 2
-#define NAV 3
-#define FNC 4
-
 #define _BASE \
 	KC_TAB,  Q_TH,    W_TH,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
 	KC_GESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
 	OSM_SFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    M_TH,    COMM_TH, DOT_TH,  SLSH_TH, KC_ENT,  \
-	       RSA_T(KC_DEL), LT(NAV,KC_BSPC), RSG_T(KC_ENT),     RSFT_T(KC_SPC), LT(NUM,KC_TAB), RAG_T(KC_ESC)
+	        RSA_T(KC_ESC), LT(NAV,KC_TAB), RSG_T(KC_ENT),     RSFT_T(KC_SPC), LT(NUM,KC_BSPC), RAG_T(KC_DEL)
 #define _COLE \
 	_______, _______, _______, KC_F,    KC_P,    KC_G,        KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, _______, \
 	_______, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,        _______, KC_N,    KC_E,    KC_I,    KC_O,    _______, \
 	_______, _______, _______, _______, _______, _______,     KC_K,    _______, _______, _______, _______, _______, \
 	                           _______, _______, _______,     _______, _______, _______
 #define _NUMB \
-	_______, _______, KC_1,    KC_2,    KC_3,    _______,     _______, KC_LPRN, KC_RPRN, _______, KC_DQUO, _______, \
-	_______, _______, KC_4,    KC_5,    KC_6,    _______,     _______, KC_LCBR, KC_RCBR, _______, KC_QUOT, _______, \
-	_______, _______, KC_7,    KC_8,    KC_9,    KC_0,        _______, KC_LBRC, KC_RBRC, _______, _______, _______, \
+	_______, _______, KC_1,    KC_2,    KC_3,    _______,     KC_VOLU, KC_LPRN, KC_RPRN, KC_RBRC, KC_DQUO, _______, \
+	_______, _______, KC_4,    KC_5,    KC_6,    _______,     KC_VOLD, KC_LCBR, KC_LPRN, KC_LBRC, KC_QUOT, _______, \
+	_______, _______, KC_7,    KC_8,    KC_9,    KC_0,        _______, _______, _______, _______, _______, _______, \
 	                           _______, MO(FNC), _______,     _______, _______, _______
 #define _NAVI \
 	_______, _______, KC_CIRC, KC_AMPR, KC_ASTR, _______,     KC_PGUP, KC_MINS, KC_EQL,  KC_BSLS, KC_TILD, _______, \
 	_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_GRV,  _______, \
 	KC_CAPS, _______, _______, _______, _______, _______,     KC_PGDN, KC_UNDS, KC_PLUS, KC_PIPE, _______, _______, \
-	                           _______, _______, _______,     KC_VOLD, MO(FNC), KC_VOLU
+	                           _______, _______, _______,     _______, MO(FNC), _______
 #define _FUNC \
 	_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_HOME, KC_WH_U, KC_WH_D, KC_INS,  _______, _______, \
 	_______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, \
@@ -96,8 +95,8 @@
 // 3w6 (git pull https://github.com/weteor/qmk_firmware.git 3w6_rev_2)
 #define FIVECOL_wrapper(...) LAYOUT(__VA_ARGS__)
 // Corne to 3w6 layout conversion
-#define C_3W6(k) SPLIT_3x6_3_TO_3x5_3_(k)
-#define SPLIT_3x6_3_TO_3x5_3_( \
+#define C_3W6(k) SPLIT_3x6_3_TO_3x5_3(k)
+#define SPLIT_3x6_3_TO_3x5_3( \
 	k01, k02, k03, k04, k05, k06, k07, k08, k09, k10, k11, k12, \
 	k13, k14, k15, k16, k17, k18, k19, k20, k21, k22, k23, k24, \
 	k25, k26, k27, k28, k29, k30, k31, k32, k33, k34, k35, k36, \
@@ -108,52 +107,11 @@
 	     k26, k27, k28, k29, k30, k31, k32, k33, k34, k35, \
 	               k37, k38, k39, k40, k41, k42
 
-// 3w6 3x5_3
-#define HRM36(k) HRM36_SACG(k)
-#define HRM36_SACG( \
-	k01, k02, k03, k04, k05, k06, k07, k08, k09, k10, \
-	k11, k12, k13, k14, k15, k16, k17, k18, k19, k20, \
-	k21, k22, k23, k24, k25, k26, k27, k28, k29, k30, \
-	k31, k32, k33, k34, k35, k36 \
-) \
-	k01, k02, k03, k04, k05, k06, k07, k08, k09, k10, \
-	SFT_T(k11), ALT_T(k12), CTL_T(k13), GUI_T(k14), k15, \
-	k16, GUI_T(k17), CTL_T(k18), ALT_T(k19), SFT_T(k20), \
-	k21, k22, k23, k24, k25, k26, k27, k28, k29, k30, \
-	k31, k32, k33, k34, k35, k36
-
-#define W_QWER \
-	Q_TH,    W_TH,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
-	KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
-	KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    M_TH,    COMM_TH, DOT_TH,  SLSH_TH, \
-    RSA_T(KC_DEL), LT(NAV,KC_BSPC), RSG_T(KC_ENT),   RSFT_T(KC_SPC), LT(NUM,KC_TAB), RAG_T(KC_ESC)
-#define W_COLE \
-	_______, _______, KC_F,    KC_P,    KC_G,        KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, \
-	KC_A,    KC_R,    KC_S,    KC_T,    KC_D,        _______, KC_N,    KC_E,    KC_I ,   KC_O,    \
-	_______, _______, _______, _______, _______,     KC_K,    _______, _______, _______, _______, \
-	                  _______, _______, _______,     _______, _______, _______
-#define W_NUMB \
-	_______, KC_1,    KC_2,    KC_3,    _______,     _______, KC_LPRN, KC_RPRN, _______, KC_DQUO, \
-	_______, KC_4,    KC_5,    KC_6,    _______,     _______, KC_LCBR, KC_RCBR, _______, KC_QUOT, \
-	_______, KC_7,    KC_8,    KC_9,    KC_0,        _______, KC_LBRC, KC_RBRC, _______, _______, \
-	                 _______, MO(FNC), _______,      _______, _______, _______
-#define W_NAVI \
-	_______, KC_CIRC, KC_AMPR, KC_ASTR, _______,     KC_PGUP, KC_MINS, KC_EQL,  KC_BSLS, KC_TILD, \
-	KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_GRV,  \
-	KC_CAPS, _______, _______, _______, _______,     KC_PGDN, KC_UNDS, KC_PLUS, KC_PIPE, _______, \
-		              _______, _______, _______,     KC_VOLD, MO(FNC), KC_VOLU
-#define W_FUNC \
-	KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_HOME, KC_WH_U, KC_WH_D, KC_INS,  _______, \
-	KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, \
-	Z_SLEEP, _______, _______, Z_PASTE, _______,     KC_END,  KC_BTN2, KC_BTN1, KC_DEL,  _______, \
-	                    RESET, _______, _______,     _______, _______, TG(CMK)
-
 
 
 // Planck 4x12
 #define PLANCK4x12_wrapper(...) LAYOUT_ortho_4x12(__VA_ARGS__)
-
-// // Corne to Planck layout conversion with top 3x12 keys
+// Corne to Planck 3x12 layout conversion
 #define C_P(k) SPLIT_3x6_3_TO_3x12(k)
 #define SPLIT_3x6_3_TO_3x12( \
 	k01, k02, k03, k04, k05, k06, k07, k08, k09, k10, k11, k12, \
@@ -175,7 +133,7 @@
 
 // Boardsource The Mark 65
 #define MARK65_wrapper(...) LAYOUT_all(__VA_ARGS__)
-
+// Home row mod wrapper
 #define HRM65(k) HRM65_SACG(k)
 #define HRM65_SACG( \
 	k000, k001, k002, k003, k004, k005, k006, k007, k008, k009, k010, k011, k012, k013, k014, k015, \
