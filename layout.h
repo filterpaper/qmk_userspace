41bdbd237cf25681f87fc5618c18dcd5ae6c017f
filterpaper <filterpaper@localhost>
2021-08-05 15:35:20 +0800

    Add support for ortho Technik keyboard.

--- layout.h
+++ layout.h
@@ -109,10 +109,10 @@
 
 
 
-// Planck 4x12
-#define PLANCK4x12_wrapper(...) LAYOUT_ortho_4x12(__VA_ARGS__)
-// Corne to Planck 3x12 layout conversion
-#define C_P(k) SPLIT_3x6_3_TO_3x12(k)
+// Ortho 4x12
+#define ORTHO_wrapper(...) LAYOUT_ortho_4x12(__VA_ARGS__)
+// Corne to Ortho 3x12 layout conversion
+#define C_O(k) SPLIT_3x6_3_TO_3x12(k)
 #define SPLIT_3x6_3_TO_3x12( \
 	k01, k02, k03, k04, k05, k06, k07, k08, k09, k10, k11, k12, \
 	k13, k14, k15, k16, k17, k18, k19, k20, k21, k22, k23, k24, \
