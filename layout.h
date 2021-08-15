00facf856ba45301d6d47150a65119c9324ad041
filterpaper <filterpaper@localhost>
2021-08-15 20:31:52 +0800

    Add OS X shortcuts to layers.

--- layout.h
+++ layout.h
@@ -91,7 +91,7 @@
 #define _NUMB \
 	_______, _______, KC_1,    KC_2,    KC_3,    _______,     KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_DQUO, _______, \
 	_______, _______, KC_4,    KC_5,    KC_6,    _______,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, _______, \
-	_______, _______, KC_7,    KC_8,    KC_9,    KC_0,        KC_INS,  _______, _______, _______, _______, _______, \
+	_______, _______, KC_7,    KC_8,    KC_9,    KC_0,        KC_INS,  Z_PST,   Z_CPY,   Z_CUT,   Z_UND,   _______, \
 	                           _______, MO(FNC), _______,     _______, _______, _______
 #define _SYMB \
 	_______, _______, KC_LBRC, KC_LPRN, KC_LCBR, _______,     KC_VOLU, KC_RCBR, KC_RPRN, KC_RBRC, KC_TILD, _______, \
@@ -101,7 +101,7 @@
 #define _FUNC \
 	_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       _______, KC_WH_U, KC_WH_D, _______, TG(CMK), _______, \
 	_______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, \
-	_______, Z_SLEEP, _______, _______, KC_F11,  KC_F12,      _______, KC_BTN2, KC_BTN1, _______, Z_SHUT,  _______, \
+	_______, Z_SLEEP, KC_F11,  KC_F12,  Z_PASTE, _______,     _______, KC_BTN2, KC_BTN1, _______, Z_SHUT,  _______, \
 	                             RESET, _______, _______,     _______, _______, _______
 
 
