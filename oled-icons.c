d0bfdb2f77592ca51d832dd386485742d38c09ee
filterpaper <filterpaper@localhost>
2021-09-06 10:16:26 +0800

    Use function variables to reduce firmware space.

--- oled-icons.c
+++ oled-icons.c
@@ -26,16 +26,24 @@ static void render_logo(void) {
 	static char const corne_logo[] PROGMEM = {
 		0x80, 0x81, 0x82, 0x83, 0x84,
 		0xa0, 0xa1, 0xa2, 0xa3, 0xa4,
+		0xc0, 0xc1, 0xc2, 0xc3, 0xc4,
+		0x20, 0xd1, 0xd2, 0xd3, 0x20, 0};
+
+	oled_write_P(corne_logo, false);
+
+/*	static char const corne_logo[] PROGMEM = {
+		0x80, 0x81, 0x82, 0x83, 0x84,
+		0xa0, 0xa1, 0xa2, 0xa3, 0xa4,
 		0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0};
 	static char const katakana[] PROGMEM = {
 		0x20, 0xd1, 0xd2, 0xd3, 0x20, 0};
 
 	oled_write_P(corne_logo, false);
-	oled_write_P(layer_state_is(CMK) ? PSTR("corne") : katakana, false);
+	oled_write_P(layer_state ? PSTR("corne") : katakana, false);*/
 }
 
 
-static void render_layer_state(void) {
+static void render_layer_state(uint8_t const state) {
 	static char const base_layer[] PROGMEM = {
 		0x20, 0x9a, 0x9b, 0x9c, 0x20,
 		0x20, 0xba, 0xbb, 0xbc, 0x20,
@@ -53,7 +61,7 @@ static void render_layer_state(void) {
 		0x20, 0xbd, 0xbe, 0xbf, 0x20,
 		0x20, 0xdd, 0xde, 0xdf, 0x20, 0};
 
-	switch(get_highest_layer(layer_state|default_layer_state)) {
+	switch(state) {
 		case FNC: oled_write_P(func_layer, false); break;
 		case SYM: oled_write_P(symb_layer, false); break;
 		case NUM: oled_write_P(numb_layer, false); break;
@@ -62,7 +70,7 @@ static void render_layer_state(void) {
 }
 
 
-static void render_gui_alt(uint8_t const mods) {
+static void render_gui_alt(uint8_t const gui, uint8_t const alt) {
 	static char const gui_off_1[] PROGMEM = {0x85, 0x86, 0};
 	static char const gui_off_2[] PROGMEM = {0xa5, 0xa6, 0};
 	static char const gui_on_1[] PROGMEM = {0x8d, 0x8e, 0};
@@ -84,24 +92,24 @@ static void render_gui_alt(uint8_t const mods) {
 	static char const on_on_2[] PROGMEM = {0xcc, 0};
 
 	// Top half with in between fillers
-	oled_write_P(mods & MOD_MASK_GUI ? gui_on_1 : gui_off_1, false);
-	if (mods & MOD_MASK_GUI && mods & MOD_MASK_ALT) { oled_write_P(on_on_1, false); }
-	else if (mods & MOD_MASK_GUI) { oled_write_P(on_off_1, false); }
-	else if (mods & MOD_MASK_ALT) { oled_write_P(off_on_1, false); }
+	oled_write_P(gui ? gui_on_1 : gui_off_1, false);
+	if (gui && alt) { oled_write_P(on_on_1, false); }
+	else if (gui) { oled_write_P(on_off_1, false); }
+	else if (alt) { oled_write_P(off_on_1, false); }
 	else { oled_write_P(off_off_1, false); }
-	oled_write_P(mods & MOD_MASK_ALT ? alt_on_1 : alt_off_1, false);
+	oled_write_P(alt ? alt_on_1 : alt_off_1, false);
 
 	// Bottom half with in between fillers
-	oled_write_P(mods & MOD_MASK_GUI ? gui_on_2 : gui_off_2, false);
-	if (mods & MOD_MASK_GUI & MOD_MASK_ALT) { oled_write_P(on_on_2, false); }
-	else if (mods & MOD_MASK_GUI) { oled_write_P(on_off_2, false); }
-	else if (mods & MOD_MASK_ALT) { oled_write_P(off_on_2, false); }
+	oled_write_P(gui ? gui_on_2 : gui_off_2, false);
+	if (gui && alt) { oled_write_P(on_on_2, false); }
+	else if (gui) { oled_write_P(on_off_2, false); }
+	else if (alt) { oled_write_P(off_on_2, false); }
 	else { oled_write_P(off_off_2, false); }
-	oled_write_P(mods & MOD_MASK_ALT ? alt_on_2 : alt_off_2, false);
+	oled_write_P(alt ? alt_on_2 : alt_off_2, false);
 }
 
 
-static void render_ctrl_shift(uint8_t const mods, bool const caps) {
+static void render_ctrl_shift(uint8_t const ctrl, uint8_t const shift) {
 	static char const ctrl_off_1[] PROGMEM = {0x89, 0x8a, 0};
 	static char const ctrl_off_2[] PROGMEM = {0xa9, 0xaa, 0};
 	static char const ctrl_on_1[] PROGMEM = {0x91, 0x92, 0};
@@ -123,20 +131,20 @@ static void render_ctrl_shift(uint8_t const mods, bool const caps) {
 	static char const on_on_2[] PROGMEM = {0xcc, 0};
 
 	// Top half with in between fillers
-	oled_write_P(mods & MOD_MASK_CTRL ? ctrl_on_1 : ctrl_off_1, false);
-	if (mods & MOD_MASK_CTRL && (mods & MOD_MASK_SHIFT || caps)) { oled_write_P(on_on_1, false); }
-	else if (mods & MOD_MASK_CTRL) { oled_write_P(on_off_1, false); }
-	else if (mods & MOD_MASK_SHIFT || caps) { oled_write_P(off_on_1, false); }
+	oled_write_P(ctrl ? ctrl_on_1 : ctrl_off_1, false);
+	if (ctrl && shift) { oled_write_P(on_on_1, false); }
+	else if (ctrl) { oled_write_P(on_off_1, false); }
+	else if (shift) { oled_write_P(off_on_1, false); }
 	else { oled_write_P(off_off_1, false); }
-	oled_write_P(mods & MOD_MASK_SHIFT || caps ? shift_on_1 : shift_off_1, false);
+	oled_write_P(shift ? shift_on_1 : shift_off_1, false);
 
 	// Bottom half with in between fillers
-	oled_write_P(mods & MOD_MASK_CTRL ? ctrl_on_2 : ctrl_off_2, false);
-	if (mods & MOD_MASK_CTRL && (mods & MOD_MASK_SHIFT || caps)) { oled_write_P(on_on_2, false); }
-	else if (mods & MOD_MASK_CTRL) { oled_write_P(on_off_2, false); }
-	else if (mods & MOD_MASK_SHIFT || caps) { oled_write_P(off_on_2, false); }
+	oled_write_P(ctrl ? ctrl_on_2 : ctrl_off_2, false);
+	if (ctrl && shift) { oled_write_P(on_on_2, false); }
+	else if (ctrl) { oled_write_P(on_off_2, false); }
+	else if (shift) { oled_write_P(off_on_2, false); }
 	else { oled_write_P(off_off_2, false); }
-	oled_write_P(mods & MOD_MASK_SHIFT || caps ? shift_on_2 : shift_off_2, false);
+	oled_write_P(shift ? shift_on_2 : shift_off_2, false);
 }
 
 
@@ -144,8 +152,15 @@ static void render_ctrl_shift(uint8_t const mods, bool const caps) {
 void render_mod_status(void) {
 		render_logo();
 		oled_set_cursor(0,6);
-		render_layer_state();
+		render_layer_state(get_highest_layer(layer_state|default_layer_state));
 		oled_set_cursor(0,11);
-		render_gui_alt(get_mods()|get_oneshot_mods());
-		render_ctrl_shift(get_mods()|get_oneshot_mods(), host_keyboard_led_state().caps_lock);
+		render_gui_alt(
+			(get_mods()|get_oneshot_mods()) & MOD_MASK_GUI,
+			(get_mods()|get_oneshot_mods()) & MOD_MASK_ALT
+		);
+		render_ctrl_shift(
+			(get_mods()|get_oneshot_mods()) & MOD_MASK_CTRL,
+			(get_mods()|get_oneshot_mods()) & MOD_MASK_SHIFT ||
+			host_keyboard_led_state().caps_lock
+		);
 }
