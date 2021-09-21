9bd64da503b6a1fd0ea7f40187c09fc7a12e3dcf
filterpaper <filterpaper@localhost>
2021-09-21 10:49:21 +0800

    Add spacing for legibility.

--- oled-icons.c
+++ oled-icons.c
@@ -73,38 +73,38 @@ static void render_layer_state(uint8_t const state) {
 static void render_gui_alt(uint8_t const gui, uint8_t const alt) {
 	static char const gui_off_1[] PROGMEM = {0x85, 0x86, 0};
 	static char const gui_off_2[] PROGMEM = {0xa5, 0xa6, 0};
-	static char const gui_on_1[] PROGMEM = {0x8d, 0x8e, 0};
-	static char const gui_on_2[] PROGMEM = {0xad, 0xae, 0};
+	static char const gui_on_1[]  PROGMEM = {0x8d, 0x8e, 0};
+	static char const gui_on_2[]  PROGMEM = {0xad, 0xae, 0};
 
 	static char const alt_off_1[] PROGMEM = {0x87, 0x88, 0};
 	static char const alt_off_2[] PROGMEM = {0xa7, 0xa8, 0};
-	static char const alt_on_1[] PROGMEM = {0x8f, 0x90, 0};
-	static char const alt_on_2[] PROGMEM = {0xaf, 0xb0, 0};
+	static char const alt_on_1[]  PROGMEM = {0x8f, 0x90, 0};
+	static char const alt_on_2[]  PROGMEM = {0xaf, 0xb0, 0};
 
 	// Fillers between icon frames
 	static char const off_off_1[] PROGMEM = {0xc5, 0};
 	static char const off_off_2[] PROGMEM = {0xc6, 0};
-	static char const on_off_1[] PROGMEM = {0xc7, 0};
-	static char const on_off_2[] PROGMEM = {0xc8, 0};
-	static char const off_on_1[] PROGMEM = {0xc9, 0};
-	static char const off_on_2[] PROGMEM = {0xca, 0};
-	static char const on_on_1[] PROGMEM = {0xcb, 0};
-	static char const on_on_2[] PROGMEM = {0xcc, 0};
+	static char const on_off_1[]  PROGMEM = {0xc7, 0};
+	static char const on_off_2[]  PROGMEM = {0xc8, 0};
+	static char const off_on_1[]  PROGMEM = {0xc9, 0};
+	static char const off_on_2[]  PROGMEM = {0xca, 0};
+	static char const on_on_1[]   PROGMEM = {0xcb, 0};
+	static char const on_on_2[]   PROGMEM = {0xcc, 0};
 
 	// Top half with in between fillers
 	oled_write_P(gui ? gui_on_1 : gui_off_1, false);
-	if (gui && alt) { oled_write_P(on_on_1, false); }
-	else if (gui) { oled_write_P(on_off_1, false); }
-	else if (alt) { oled_write_P(off_on_1, false); }
-	else { oled_write_P(off_off_1, false); }
+	if (gui && alt) { oled_write_P(on_on_1,   false); }
+	else if (gui)   { oled_write_P(on_off_1,  false); }
+	else if (alt)   { oled_write_P(off_on_1,  false); }
+	else            { oled_write_P(off_off_1, false); }
 	oled_write_P(alt ? alt_on_1 : alt_off_1, false);
 
 	// Bottom half with in between fillers
 	oled_write_P(gui ? gui_on_2 : gui_off_2, false);
-	if (gui && alt) { oled_write_P(on_on_2, false); }
-	else if (gui) { oled_write_P(on_off_2, false); }
-	else if (alt) { oled_write_P(off_on_2, false); }
-	else { oled_write_P(off_off_2, false); }
+	if (gui && alt) { oled_write_P(on_on_2,   false); }
+	else if (gui)   { oled_write_P(on_off_2,  false); }
+	else if (alt)   { oled_write_P(off_on_2,  false); }
+	else            { oled_write_P(off_off_2, false); }
 	oled_write_P(alt ? alt_on_2 : alt_off_2, false);
 }
 
@@ -112,38 +112,38 @@ static void render_gui_alt(uint8_t const gui, uint8_t const alt) {
 static void render_ctrl_shift(uint8_t const ctrl, uint8_t const shift) {
 	static char const ctrl_off_1[] PROGMEM = {0x89, 0x8a, 0};
 	static char const ctrl_off_2[] PROGMEM = {0xa9, 0xaa, 0};
-	static char const ctrl_on_1[] PROGMEM = {0x91, 0x92, 0};
-	static char const ctrl_on_2[] PROGMEM = {0xb1, 0xb2, 0};
+	static char const ctrl_on_1[]  PROGMEM = {0x91, 0x92, 0};
+	static char const ctrl_on_2[]  PROGMEM = {0xb1, 0xb2, 0};
 
 	static char const shift_off_1[] PROGMEM = {0x8b, 0x8c, 0};
 	static char const shift_off_2[] PROGMEM = {0xab, 0xac, 0};
-	static char const shift_on_1[] PROGMEM = {0xcd, 0xce, 0};
-	static char const shift_on_2[] PROGMEM = {0xcf, 0xd0, 0};
+	static char const shift_on_1[]  PROGMEM = {0xcd, 0xce, 0};
+	static char const shift_on_2[]  PROGMEM = {0xcf, 0xd0, 0};
 
 	// Fillers between icon frames
 	static char const off_off_1[] PROGMEM = {0xc5, 0};
 	static char const off_off_2[] PROGMEM = {0xc6, 0};
-	static char const on_off_1[] PROGMEM = {0xc7, 0};
-	static char const on_off_2[] PROGMEM = {0xc8, 0};
-	static char const off_on_1[] PROGMEM = {0xc9, 0};
-	static char const off_on_2[] PROGMEM = {0xca, 0};
-	static char const on_on_1[] PROGMEM = {0xcb, 0};
-	static char const on_on_2[] PROGMEM = {0xcc, 0};
+	static char const on_off_1[]  PROGMEM = {0xc7, 0};
+	static char const on_off_2[]  PROGMEM = {0xc8, 0};
+	static char const off_on_1[]  PROGMEM = {0xc9, 0};
+	static char const off_on_2[]  PROGMEM = {0xca, 0};
+	static char const on_on_1[]   PROGMEM = {0xcb, 0};
+	static char const on_on_2[]   PROGMEM = {0xcc, 0};
 
 	// Top half with in between fillers
 	oled_write_P(ctrl ? ctrl_on_1 : ctrl_off_1, false);
-	if (ctrl && shift) { oled_write_P(on_on_1, false); }
-	else if (ctrl) { oled_write_P(on_off_1, false); }
-	else if (shift) { oled_write_P(off_on_1, false); }
-	else { oled_write_P(off_off_1, false); }
+	if (ctrl && shift) { oled_write_P(on_on_1,   false); }
+	else if (ctrl)     { oled_write_P(on_off_1,  false); }
+	else if (shift)    { oled_write_P(off_on_1,  false); }
+	else               { oled_write_P(off_off_1, false); }
 	oled_write_P(shift ? shift_on_1 : shift_off_1, false);
 
 	// Bottom half with in between fillers
 	oled_write_P(ctrl ? ctrl_on_2 : ctrl_off_2, false);
-	if (ctrl && shift) { oled_write_P(on_on_2, false); }
-	else if (ctrl) { oled_write_P(on_off_2, false); }
-	else if (shift) { oled_write_P(off_on_2, false); }
-	else { oled_write_P(off_off_2, false); }
+	if (ctrl && shift) { oled_write_P(on_on_2,   false); }
+	else if (ctrl)     { oled_write_P(on_off_2,  false); }
+	else if (shift)    { oled_write_P(off_on_2,  false); }
+	else               { oled_write_P(off_off_2, false); }
 	oled_write_P(shift ? shift_on_2 : shift_off_2, false);
 }
 
