1abc08a150d748583bb190bb559e2928b9f695d6
filterpaper <filterpaper@localhost>
2021-11-20 20:09:30 +0800

    Use SPDX license format

--- oled-luna.c
+++ oled-luna.c
@@ -1,18 +1,5 @@
-/* Copyright (C) 2021 @filterpaper
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Copyright 2021 @filterpaper
+// SPDX-License-Identifier: GPL-2.0+
 
 /* Graphical Luna / Felix animation, driven by key press timer or WPM.
    Frames are 4x3 on OLED and oriented for OLED_ROTATION_270.
