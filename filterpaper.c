84f3a04ad724b4e2ec8b803ca6974d07c3381d2f
filterpaper <filterpaper@localhost>
2021-11-29 21:03:28 +0800

    Remove swap hands code added to QMK repo.

--- filterpaper.c
+++ filterpaper.c
@@ -9,38 +9,6 @@ uint32_t tap_timer = 0; // Timer for OLED animation
 #endif
 
 
-#ifdef SWAP_HANDS_ENABLE
-#if defined(KEYBOARD_a_dux) || defined(KEYBOARD_cradio)
-const keypos_t PROGMEM hand_swap_config[MATRIX_ROWS][MATRIX_COLS] = {
-	// Left
-	{{4, 4}, {3, 4}, {2, 4}, {1, 4}, {0, 4}},
-	{{4, 5}, {3, 5}, {2, 5}, {1, 5}, {0, 5}},
-	{{4, 6}, {3, 6}, {2, 6}, {1, 6}, {0, 6}},
-	{{1, 7}, {0, 7}, {2, 7}, {3, 7}, {4, 7}},
-	// Right
-	{{4, 0}, {3, 0}, {2, 0}, {1, 0}, {0, 0}},
-	{{4, 1}, {3, 1}, {2, 1}, {1, 1}, {0, 1}},
-	{{4, 2}, {3, 2}, {2, 2}, {1, 2}, {0, 2}},
-	{{1, 3}, {0, 3}, {2, 3}, {3, 3}, {4, 3}}
-};
-#endif
-#if defined(KEYBOARD_3w6_rev2)
-const keypos_t PROGMEM hand_swap_config[MATRIX_ROWS][MATRIX_COLS] = {
-	// Left
-	{{4, 4}, {3, 4}, {2, 4}, {1, 4}, {0, 4}},
-	{{4, 5}, {3, 5}, {2, 5}, {1, 5}, {0, 5}},
-	{{4, 6}, {3, 6}, {2, 6}, {1, 6}, {0, 6}},
-	{{4, 7}, {3, 7}, {2, 7}, {1, 7}, {0, 7}},
-	// Right
-	{{4, 0}, {3, 0}, {2, 0}, {1, 0}, {0, 0}},
-	{{4, 1}, {3, 1}, {2, 1}, {1, 1}, {0, 1}},
-	{{4, 2}, {3, 2}, {2, 2}, {1, 2}, {0, 2}},
-	{{4, 3}, {3, 3}, {2, 3}, {1, 3}, {0, 3}}
-};
-#endif
-#endif
-
-
 // Tap hold customization
 #ifdef TAPPING_FORCE_HOLD_PER_KEY // Enable for right thumb keys
 bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
