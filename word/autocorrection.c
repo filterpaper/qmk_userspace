a2cead9de2325279b292f14805d365950ec6dbc5
filterpaper <filterpaper@localhost>
2021-12-08 12:49:33 +0800

    Remove unneeded 0 on last buffer element, more succinct comments

--- word/autocorrection.c
+++ word/autocorrection.c
@@ -11,22 +11,17 @@ bool process_autocorrection(uint16_t keycode, keyrecord_t* record) {
 	static uint8_t typo_buffer[DICTIONARY_MAX_LENGTH] = {0};
 	static uint8_t typo_buffer_size = 0;
 
-	// Disable autocorrection while a mod other than shift is active.
+	// Disable while a mod other than shift is active.
 	if ((get_mods() & ~MOD_MASK_SHIFT) != 0) {
 		typo_buffer_size = 0;
 		return true;
 	}
 
-	// Handle non-alpha key.
+	// Reduce buffer for backspace key, disable for other non-alpha.
 	uint8_t const basekey = keycode & 0xff;
 	if (!(KC_A <= basekey && basekey <= KC_Z) && basekey != KC_SPC) {
-		if (basekey == KC_BSPC && typo_buffer_size > 0) {
-			// Remove last character from buffer.
-			--typo_buffer_size;
-		} else {
-			// Clear state for other non-alpha key.
-			typo_buffer_size = 0;
-		}
+		if (basekey == KC_BSPC && typo_buffer_size > 0) { typo_buffer_size--; }
+		else { typo_buffer_size = 0; }
 		return true;
 	}
 
@@ -38,11 +33,8 @@ bool process_autocorrection(uint16_t keycode, keyrecord_t* record) {
 
 	// Append `keycode` to buffer.
 	typo_buffer[typo_buffer_size++] = (uint8_t)keycode;
-	if (typo_buffer_size < DICTIONARY_MAX_LENGTH) {
-		typo_buffer[typo_buffer_size] = 0;
-		// Early return if not many characters have been buffered so far.
-		if (typo_buffer_size < DICTIONARY_MIN_LENGTH) { return true; }
-	}
+	// Return if buffer is less than shortest word.
+	if (typo_buffer_size < DICTIONARY_MIN_LENGTH) { return true; }
 
 	// Check for typo in buffer using a trie stored in `dictionary`.
 	uint16_t state = 0;
@@ -53,14 +45,14 @@ bool process_autocorrection(uint16_t keycode, keyrecord_t* record) {
 		if (code & 128) {  // Check for match in node with multiple children.
 			code &= 127;
 			for (; code != buffer; code = dictionary[state += 3]) {
-				if (!code) { return true; }
+				if (!code) { return true; }  // No parent match
 			}
 			// Follow link to child node.
 			state = (dictionary[state + 1] | dictionary[state + 2] << 8);
 			if ((state & 0x8000) != 0) { goto found_typo; }
 		// Check for match in node with single child.
 		} else if (code != buffer) {
-			return true;
+			return true;  // No child match
 		} else if (!dictionary[++state] && !(dictionary[++state] & 128)) {
 			goto found_typo;
 		}
