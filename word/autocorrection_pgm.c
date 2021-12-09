6766c6215400455d6312993c1d88c685c6adcdff
filterpaper <filterpaper@localhost>
2021-12-09 18:39:37 +0800

    Add test PROGMEM version

---
+++ word/autocorrection_pgm.c
@@ -0,0 +1,77 @@
+// Copyright 2021 Google LLC
+// Copyright 2022 @filterpaper
+// SPDX-License-Identifier: Apache-2.0
+// Original source: https://getreuer.info/posts/keyboards/autocorrection
+
+#include QMK_KEYBOARD_H
+#include <string.h>
+#include "autocorrection_data_pgm.h"
+
+bool process_autocorrection(uint16_t keycode, keyrecord_t* record) {
+	static uint8_t typo_buffer[DICTIONARY_MAX_LENGTH] = {0};
+	static uint8_t buffer_size = 0;
+
+	// Exclude Shift hold from resetting autocorrection.
+	if (keycode == KC_LSFT || keycode == KC_RSFT ||
+	(QK_MOD_TAP <= keycode && keycode <= QK_MOD_TAP_MAX &&
+	((keycode >> 8) & 0x0f) == MOD_LSFT && !record->tap.count)) {
+		return true;
+	}
+
+	// Subtract buffer for Backspace key, reset for other non-alpha.
+	if (!(KC_A <= (uint8_t)keycode && (uint8_t)keycode <= KC_Z) && (uint8_t)keycode != KC_SPC) {
+		if ((uint8_t)keycode == KC_BSPC && buffer_size) { buffer_size--; }
+		else { buffer_size = 0; }
+		return true;
+	}
+
+	// Rotate oldest character if buffer is full.
+	if (buffer_size >= DICTIONARY_MAX_LENGTH) {
+		memmove(typo_buffer, typo_buffer + 1, DICTIONARY_MAX_LENGTH - 1);
+		buffer_size = DICTIONARY_MAX_LENGTH - 1;
+	}
+
+	// Append `keycode` to buffer.
+	typo_buffer[buffer_size++] = (uint8_t)keycode;
+	// Return if buffer is smaller than the shortest word.
+	if (buffer_size < DICTIONARY_MIN_LENGTH) { return true; }
+
+	// Check for typo in buffer using a trie stored in `dictionary`.
+	uint16_t state = 0;
+	for (uint8_t i = buffer_size - 1; i >= 0; --i) {
+		uint8_t const buffer = typo_buffer[i];
+		uint8_t code = pgm_read_byte(dictionary+state);
+
+		if (code & 128) {  // Check for match in node with multiple children.
+			code &= 127;
+			for (; code != buffer; code = pgm_read_byte(dictionary+(state+=3))) {
+				if (!code) { return true; }
+			}
+			// Follow link to child node.
+			state = (pgm_read_byte(dictionary + state + 1) | pgm_read_byte(dictionary + state + 2) << 8);
+			if ((state & 0x8000) != 0) { goto found_typo; }
+		// Check for match in node with single child.
+		} else if (code != buffer) {
+			return true;
+		} else if (!pgm_read_byte(dictionary + (++state)) && !(pgm_read_byte(dictionary + (++state)) & 128)) {
+			goto found_typo;
+		}
+	}
+	return true;
+
+found_typo:  // A typo was found! Apply autocorrection.
+	state &= 0x7fff;
+	uint8_t const backspaces = pgm_read_byte(dictionary + state);
+	for (uint8_t i = 0; i < backspaces; ++i) { tap_code(KC_BSPC); }
+//	send_string((char const *)(dictionary + state + 1));
+	send_string((char const *)pgm_read_byte_near(&dictionary[1304]));
+
+	if (keycode == KC_SPC) {
+		typo_buffer[0] = KC_SPC;
+		buffer_size = 1;
+		return true;
+	} else {
+		buffer_size = 0;
+		return false;
+	}
+}
