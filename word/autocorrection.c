8b6a3cda0dfaee99fd891c9e211750fdb316b3c9
filterpaper <filterpaper@localhost>
2021-12-09 10:24:27 +0800

    Typecast keycode for comparison to avoid excess variable use

--- word/autocorrection.c
+++ word/autocorrection.c
@@ -9,37 +9,36 @@
 
 bool process_autocorrection(uint16_t keycode, keyrecord_t* record) {
 	static uint8_t typo_buffer[DICTIONARY_MAX_LENGTH] = {0};
-	static uint8_t typo_buffer_size = 0;
+	static uint8_t buffer_size = 0;
 
-	// Exclude Shift press from reseting autocorrection
+	// Exclude Shift hold from resetting autocorrection.
 	if (keycode == KC_LSFT || keycode == KC_RSFT ||
 	(QK_MOD_TAP <= keycode && keycode <= QK_MOD_TAP_MAX &&
 	((keycode >> 8) & 0x0f) == MOD_LSFT && !record->tap.count)) {
 		return true;
 	}
 
-	// Subtract buffer for backspace key, reset for other non-alpha.
-	uint8_t const basekey = keycode & 0xff;
-	if (!(KC_A <= basekey && basekey <= KC_Z) && basekey != KC_SPC) {
-		if (basekey == KC_BSPC && typo_buffer_size > 0) { typo_buffer_size--; }
-		else { typo_buffer_size = 0; }
+	// Subtract buffer for Backspace key, reset for other non-alpha.
+	if (!(KC_A <= (uint8_t)keycode && (uint8_t)keycode <= KC_Z) && (uint8_t)keycode != KC_SPC) {
+		if ((uint8_t)keycode == KC_BSPC && buffer_size) { buffer_size--; }
+		else { buffer_size = 0; }
 		return true;
 	}
 
 	// Rotate oldest character if buffer is full.
-	if (typo_buffer_size >= DICTIONARY_MAX_LENGTH) {
+	if (buffer_size >= DICTIONARY_MAX_LENGTH) {
 		memmove(typo_buffer, typo_buffer + 1, DICTIONARY_MAX_LENGTH - 1);
-		typo_buffer_size = DICTIONARY_MAX_LENGTH - 1;
+		buffer_size = DICTIONARY_MAX_LENGTH - 1;
 	}
 
 	// Append `keycode` to buffer.
-	typo_buffer[typo_buffer_size++] = (uint8_t)keycode;
+	typo_buffer[buffer_size++] = (uint8_t)keycode;
 	// Return if buffer is smaller than the shortest word.
-	if (typo_buffer_size < DICTIONARY_MIN_LENGTH) { return true; }
+	if (buffer_size < DICTIONARY_MIN_LENGTH) { return true; }
 
 	// Check for typo in buffer using a trie stored in `dictionary`.
 	uint16_t state = 0;
-	for (uint8_t i = typo_buffer_size - 1; i >= 0; --i) {
+	for (uint8_t i = buffer_size - 1; i >= 0; --i) {
 		uint8_t const buffer = typo_buffer[i];
 		uint8_t code = dictionary[state];
 
@@ -68,10 +67,10 @@ found_typo:  // A typo was found! Apply autocorrection.
 
 	if (keycode == KC_SPC) {
 		typo_buffer[0] = KC_SPC;
-		typo_buffer_size = 1;
+		buffer_size = 1;
 		return true;
 	} else {
-		typo_buffer_size = 0;
+		buffer_size = 0;
 		return false;
 	}
 }
