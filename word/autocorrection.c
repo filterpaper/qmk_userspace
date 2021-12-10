0c5c574f8a8a1c7bce33a1764f1ccd863b9b0c67
filterpaper <filterpaper@localhost>
2021-12-10 14:50:07 +0800

    Support one-shot-shift key, increase dictionary size

--- word/autocorrection.c
+++ word/autocorrection.c
@@ -16,7 +16,12 @@ bool process_autocorrection(uint16_t keycode, keyrecord_t* record) {
 	// Exclude Shift hold from resetting autocorrection.
 	if (keycode == KC_LSFT || keycode == KC_RSFT ||
 		(QK_MOD_TAP <= keycode && keycode <= QK_MOD_TAP_MAX &&
-		((keycode >> 8) & 0x0f) == MOD_LSFT && !record->tap.count)) {
+		((keycode >> 8) & 0xf) == MOD_LSFT && !record->tap.count)
+#ifndef NO_ACTION_ONESHOT
+		|| (QK_ONE_SHOT_MOD <= keycode && keycode <= QK_ONE_SHOT_MOD_MAX
+		&& (keycode & 0xf) == MOD_LSFT)
+#endif
+	) {
 		return true;
 	}
 
