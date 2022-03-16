8605e6248592e76ca6f035ab38e2d208365cc2ad
Albert Y <76888457+filterpaper@users.noreply.github.com>
2022-03-16 20:20:02 +0800

    Improve comments

--- bilateral-mods.c
+++ bilateral-mods.c
@@ -1,7 +1,11 @@
 // Copyright 2022 @filterpaper
 // SPDX-License-Identifier: Apache-2.0
+
 /* Simple bilateral home row mod implementation that replaces activated
    modifier with the base keycode if rolled on the same half.
+
+   Code assumes left and right halves are horizontally with MATRIX_COLS/2
+   while split keyboard matrix is divided with MATRIX_ROWS/2.
 
    Usage:
    Include this source file in 'rules.mk' with:
@@ -20,9 +24,11 @@
 void process_bilateral_mod(uint16_t keycode, keyrecord_t *record) {
 
 	void process_unilateral(uint16_t kc) {
-		// Check for R mod-tap bit and shift 4-bits left for mod_bits
+		// Check for right mod-tap flag and shift 4 bits left for right mod_bit.
 		uint8_t mod_tap_bit = (kc & 0x1000) ? ((kc>>8)&0xf)<<4 : (kc>>8)&0xf;
 		if (keycode != kc && get_mods() & mod_tap_bit) {
+			// If tapped key is not the processed mod tap key and modifier
+			// is active, replace modifier with mod tap base keycode.
 			unregister_mods(mod_tap_bit);
 			tap_code(kc);
 		}
@@ -34,8 +40,10 @@ void process_bilateral_mod(uint16_t keycode, keyrecord_t *record) {
 #else
 		if (record->event.key.col < MATRIX_COLS/2) {
 #endif
-			// List of left mod taps to exclude unilateral modifier
-			process_unilateral(HM_A);
+			// Process key press on the left half. If left mod is active, assume it
+			// was rolled from the previous left mod taps listed below and replace
+			// the modifier with base keycode.
+		//	process_unilateral(HM_A);
 			process_unilateral(HM_S);
 			process_unilateral(HM_D);
 			process_unilateral(HM_F);
@@ -45,11 +53,13 @@ void process_bilateral_mod(uint16_t keycode, keyrecord_t *record) {
 #else
 		if (record->event.key.col >= MATRIX_COLS/2) {
 #endif
-			// List of right mod taps to exclude unilateral modifier
+			// Process key press on the right half. If right mod is active, assume it
+			// was rolled from the previous right mod tap listed below and replace
+			// the modifier with base keycode.
 			process_unilateral(HM_J);
 			process_unilateral(HM_K);
 			process_unilateral(HM_L);
-			process_unilateral(HM_QUOT);
+		//	process_unilateral(HM_QUOT);
 		}
 
 	}
