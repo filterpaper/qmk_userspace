1b6b68d88ce1566d6d26dfbf1ffc94ca5a406465
Albert Y <76888457+filterpaper@users.noreply.github.com>
2022-03-18 17:15:56 +0800

    Refactor mod evaluation code

--- unilateral-taps.c
+++ unilateral-taps.c
@@ -33,37 +33,65 @@
 #endif
 
 void process_unilateral_taps(uint16_t keycode, keyrecord_t *record) {
-	void process_unilateral(uint16_t kc) {
+	uint8_t get_modtap_bit(uint16_t kc) {
 		// Check for right mod-tap flag and shift 4 bits left for right mod_bit.
-		uint8_t mod_tap_bit = (kc & 0x1000) ? ((kc>>8)&0xf)<<4 : (kc>>8)&0xf;
-		if (keycode != kc && get_mods() & mod_tap_bit) {
-			// If tapped key is not the processed mod tap key and modifier
-			// is active, replace modifier with mod tap base keycode.
-			unregister_mods(mod_tap_bit);
-			tap_code(kc);
-		}
+		return (kc & 0x1000) ? ((kc>>8)&0xf)<<4 : (kc>>8)&0xf;
 	}
 
 	if ((QK_MOD_TAP <= keycode && keycode <= QK_MOD_TAP_MAX && record->tap.count) || IS_ANY(keycode)) {
-		// Left hand keys
 		if (KEY_POSITION < KB_MIDPOINT) {
 			// Process key press on the left half. If left mod is active, assume it
 			// was rolled from the previous left mod taps listed below and replace
 			// the modifier with base keycode.
-			process_unilateral(HM_A);
-			process_unilateral(HM_S);
-			process_unilateral(HM_D);
-			process_unilateral(HM_F);
+			if (get_mods() & get_modtap_bit(HM_A)) {
+				unregister_mods(get_modtap_bit(HM_A));
+				tap_code((uint8_t)HM_A);
+			} else if (get_mods() & get_modtap_bit(HM_S)) {
+				unregister_mods(get_modtap_bit(HM_S));
+				tap_code((uint8_t)HM_S);
+			} else if (get_mods() & get_modtap_bit(HM_D)) {
+				unregister_mods(get_modtap_bit(HM_D));
+				tap_code((uint8_t)HM_D);
+			} else if (get_mods() & get_modtap_bit(HM_F)) {
+				unregister_mods(get_modtap_bit(HM_F));
+				tap_code((uint8_t)HM_F);
+			}
+		/*	switch (get_mods()) {
+				case MOD_LSFT:
+					unregister_mods(get_modtap_bit(HM_A));
+					tap_code((uint8_t)HM_A);
+					break;
+				case MOD_LALT:
+					unregister_mods(get_modtap_bit(HM_S));
+					tap_code((uint8_t)HM_S);
+					break;
+				case MOD_LCTL:
+					unregister_mods(get_modtap_bit(HM_D));
+					tap_code((uint8_t)HM_D);
+					break;
+				case MOD_LGUI:
+					unregister_mods(get_modtap_bit(HM_F));
+					tap_code((uint8_t)HM_F);
+					break;
+			} */
 		}
-		// Right hand keys
 		if (KEY_POSITION >= KB_MIDPOINT) {
 			// Process key press on the right half. If right mod is active, assume it
 			// was rolled from the previous right mod tap listed below and replace
 			// the modifier with base keycode.
-			process_unilateral(HM_J);
-			process_unilateral(HM_K);
-			process_unilateral(HM_L);
-			process_unilateral(HM_QUOT);
+			if (get_mods() & get_modtap_bit(HM_J)) {
+				unregister_mods(get_modtap_bit(HM_J));
+				tap_code((uint8_t)HM_J);
+			} else if (get_mods() & get_modtap_bit(HM_K)) {
+				unregister_mods(get_modtap_bit(HM_K));
+				tap_code((uint8_t)HM_K);
+			} else if (get_mods() & get_modtap_bit(HM_L)) {
+				unregister_mods(get_modtap_bit(HM_L));
+				tap_code((uint8_t)HM_L);
+			} else if (get_mods() & get_modtap_bit(HM_QUOT)) {
+				unregister_mods(get_modtap_bit(HM_QUOT));
+				tap_code((uint8_t)HM_QUOT);
+			}
 		}
 	}
 }
