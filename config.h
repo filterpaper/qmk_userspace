970ea761b73a215f5d79cd6224975ddead8896d3
Albert Y <76888457+filterpaper@users.noreply.github.com>
2022-08-29 18:07:31 +0800

    Enable LTO, tweak mod tap and RGB

--- config.h
+++ config.h
@@ -17,57 +17,39 @@
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_TERM 220
 #define TAPPING_TERM_PER_KEY
-//#define PERMISSIVE_HOLD_PER_KEY
+#define PERMISSIVE_HOLD_PER_KEY
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 #define TAPPING_FORCE_HOLD_PER_KEY
 
 #ifdef QMK_MCU_FAMILY_RP
-#	define SPLIT_USB_TIMEOUT 2000
+#	define SPLIT_USB_TIMEOUT 3000
 #	define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
 #endif
 
 #ifdef SPLIT_KEYBOARD
-#	ifdef KEYBOARD_crkbd_rev1
-#		define BOOTMAGIC_LITE_ROW    0
-#		define BOOTMAGIC_LITE_COLUMN 0
-#		define BOOTMAGIC_LITE_ROW_RIGHT    4
-#		define BOOTMAGIC_LITE_COLUMN_RIGHT 0
-#	endif
+#	define EE_HANDS
 #	if defined(OLED_ENABLE) || defined(RGB_MATRIX_ENABLE)
 #		define SPLIT_OLED_ENABLE
 #		define SPLIT_MODS_ENABLE
 #		define SPLIT_LED_STATE_ENABLE
 #		define SPLIT_LAYER_STATE_ENABLE
 #	endif
-#	define EE_HANDS
-#	define SPLIT_MAX_CONNECTION_ERRORS 0
-#	define SPLIT_CONNECTION_CHECK_TIMEOUT 0
+#	ifdef __AVR__
+#		define SPLIT_MAX_CONNECTION_ERRORS 0
+#		define SPLIT_CONNECTION_CHECK_TIMEOUT 0
+#	endif
 #endif
 
 #ifdef MOUSEKEY_ENABLE
-#	ifndef MK_KINETIC_SPEED
-#		define MOUSEKEY_DELAY 0			// Delay between pressing a key and cursor movement
-#		define MOUSEKEY_INTERVAL 15		// Time between cursor movements in milliseconds
-#		define MOUSEKEY_MOVE_DELTA 10	// Step size for acceleration
-#		define MOUSEKEY_MAX_SPEED 9
-#		define MOUSEKEY_TIME_TO_MAX 90
-#		define MOUSEKEY_WHEEL_DELAY 16
-#		define MOUSEKEY_WHEEL_INTERVAL 30
-#		define MOUSEKEY_WHEEL_MAX_SPEED 10
-#		define MOUSEKEY_WHEEL_TIME_TO_MAX 95
-#	else
-#		define MOUSEKEY_DELAY 0
-#		define MOUSEKEY_INTERVAL 10
-#		define MOUSEKEY_MOVE_DELTA 16
-#		define MOUSEKEY_INITIAL_SPEED 100 // Initial speed of the cursor in pixel per second
-#		define MOUSEKEY_BASE_SPEED   5000 // Maximum cursor speed at which acceleration stops
-#		define MOUSEKEY_DECELERATED_SPEED  500
-#		define MOUSEKEY_ACCELERATED_SPEED 5000
-#		define MOUSEKEY_WHEEL_INITIAL_MOVEMENTS  8
-#		define MOUSEKEY_WHEEL_BASE_MOVEMENTS    32
-#		define MOUSEKEY_WHEEL_DECELERATED_MOVEMENTS  8
-#		define MOUSEKEY_WHEEL_ACCELERATED_MOVEMENTS 48
-#	endif
+#	define MOUSEKEY_DELAY 0			// Delay between pressing a key and cursor movement
+#	define MOUSEKEY_INTERVAL 15		// Time between cursor movements in milliseconds
+#	define MOUSEKEY_MOVE_DELTA 10	// Step size for acceleration
+#	define MOUSEKEY_MAX_SPEED 9
+#	define MOUSEKEY_TIME_TO_MAX 90
+#	define MOUSEKEY_WHEEL_DELAY 16
+#	define MOUSEKEY_WHEEL_INTERVAL 30
+#	define MOUSEKEY_WHEEL_MAX_SPEED 10
+#	define MOUSEKEY_WHEEL_TIME_TO_MAX 95
 #endif
 
 #ifdef RGB_MATRIX_ENABLE
@@ -81,8 +63,8 @@
 #	define RGB_MATRIX_STARTUP_SAT 135
 #	define RGB_MATRIX_KEYPRESSES
 #	ifdef RGB_MATRIX_CUSTOM_USER
+#		define ENABLE_RGB_MATRIX_CANDY_TAP
 #		define ENABLE_RGB_MATRIX_CANDY_RAIN
-#		define ENABLE_RGB_MATRIX_CANDY_FRACTAL
 #	else
 #		define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
 #		define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
