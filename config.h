4eb170eb1b730ee1cdb5f53f89abe0c9366c1588
Albert Y <76888457+filterpaper@users.noreply.github.com>
2023-03-07 07:10:28 +0800

    Disable git safe directory checks for actions
    Revert tapping term to 220
    
    Use quantum keycode macros

--- config.h
+++ config.h
@@ -16,7 +16,7 @@
 #define NO_ACTION_ONESHOT
 // Tap-hold settings
 #define IGNORE_MOD_TAP_INTERRUPT
-#define TAPPING_TERM 200
+#define TAPPING_TERM 220
 #define TAPPING_TERM_PER_KEY
 #define QUICK_TAP_TERM 110
 #define QUICK_TAP_TERM_PER_KEY
