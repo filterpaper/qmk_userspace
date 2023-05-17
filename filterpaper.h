b68cbaa85315a95f72f93d4ebae9727dee3e3949
Albert Y <76888457+filterpaper@users.noreply.github.com>
2023-05-17 10:04:22 +0800

    Add unilateral tap solution

--- filterpaper.h
+++ filterpaper.h
@@ -6,16 +6,20 @@
 
 extern void autocorrect_toggle(void);
 extern bool process_autocorrect(uint16_t keycode, keyrecord_t *record);
+static inline bool process_tap_hold(uint16_t keycode, keyrecord_t *record);
+static inline bool process_caps_unlock(uint16_t keycode, keyrecord_t *record);
 
 // Mod-tap decision helper macros
-#define IS_TYPING()    (timer_elapsed_fast(tap_timer) < TAPPING_TERM * 2)
-#define IS_HOMEROW()   (record->event.key.row == 1 || record->event.key.row == 5)
-#define IS_MT_SHIFT(k) (QK_MOD_TAP_GET_MODS(k) & MOD_MASK_SHIFT)
+#define IS_TYPING() (timer_elapsed_fast(tap_timer) < TAPPING_TERM * 2)
+#define IS_HOMEROW() (record->event.key.row == 1 || record->event.key.row == 5)
 
-// Mod-tap and the key that follows are on opposite sides
-#define IS_BILATERAL_TAP() (                             \
-	(record->event.key.row == 1 && next_key.row > 3) ||  \
-	(record->event.key.row == 5 && next_key.row < 4) )
+#define IS_UNILATERAL_TAP() ( \
+(record->event.key.row == 1 && 0 <= next_record.event.key.row && next_record.event.key.row <= 2) || \
+(record->event.key.row == 5 && 4 <= next_record.event.key.row && next_record.event.key.row <= 6) )
+
+#define IS_BILATERAL_TAP() ( \
+(record->event.key.row == 1 && 4 <= next_record.event.key.row && next_record.event.key.row <= 6) || \
+(record->event.key.row == 5 && 0 <= next_record.event.key.row && next_record.event.key.row <= 2) )
 
 #ifdef COMBO_ENABLE
 #	include "combos.h"
