6e800b52be8af51bc022ed68ba083744086e6532
Albert Y <76888457+filterpaper@users.noreply.github.com>
2023-05-18 17:25:51 +0800

    Use CAGS modifier configuration on home row
    Change mod-tap macros to support parameters

--- filterpaper.h
+++ filterpaper.h
@@ -11,15 +11,15 @@ static inline bool process_caps_unlock(uint16_t keycode, keyrecord_t *record);
 
 // Mod-tap decision helper macros
 #define IS_TYPING() (timer_elapsed_fast(tap_timer) < TAPPING_TERM * 2)
-#define IS_HOMEROW() (record->event.key.row == 1 || record->event.key.row == 5)
+#define IS_HOMEROW(r) (r->event.key.row == 1 || r->event.key.row == 5)
 
-#define IS_UNILATERAL_TAP() ( \
-(record->event.key.row == 1 && 0 <= next_record.event.key.row && next_record.event.key.row <= 2) || \
-(record->event.key.row == 5 && 4 <= next_record.event.key.row && next_record.event.key.row <= 6) )
+#define IS_UNILATERAL_TAP(r, n) ( \
+(r->event.key.row == 1 && 0 <= n.event.key.row && n.event.key.row <= 2) || \
+(r->event.key.row == 5 && 4 <= n.event.key.row && n.event.key.row <= 6) )
 
-#define IS_BILATERAL_TAP() ( \
-(record->event.key.row == 1 && 4 <= next_record.event.key.row && next_record.event.key.row <= 6) || \
-(record->event.key.row == 5 && 0 <= next_record.event.key.row && next_record.event.key.row <= 2) )
+#define IS_BILATERAL_TAP(r, n) ( \
+(r->event.key.row == 1 && 4 <= n.event.key.row && n.event.key.row <= 6) || \
+(r->event.key.row == 5 && 0 <= n.event.key.row && n.event.key.row <= 2) )
 
 #ifdef COMBO_ENABLE
 #	include "combos.h"
