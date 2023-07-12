0b4ccf86bbaf38582997094773b02be12b637e8e
フィルターペーパー <76888457+filterpaper@users.noreply.github.com>
2023-07-12 13:58:17 +0800

    Tweak hold tap configuration

--- filterpaper.h
+++ filterpaper.h
@@ -14,8 +14,9 @@ static inline bool process_caps_unlock(uint16_t keycode, keyrecord_t *record);
 
 // Tap-hold decision helper macros
 #define IS_TYPING()          (timer_elapsed_fast(tap_timer) < TAPPING_TERM * 2)
-#define IS_MOD_TAP_SHIFT(kc) (QK_MOD_TAP_GET_MODS(kc) & MOD_LSFT)
 #define IS_HOMEROW(r)        (r->event.key.row == 1 || r->event.key.row == 5)
+#define IS_MOD_TAP_SHIFT(kc) (QK_MOD_TAP_GET_MODS(kc) & MOD_LSFT)
+#define IS_MOD_TAP_CS(kc)    (QK_MOD_TAP_GET_MODS(kc) & (MOD_LCTL | MOD_LSFT))
 
 #define IS_UNILATERAL_TAP(r, n) ((r->event.key.col != n.event.key.col) && (    \
     (r->event.key.row == 1 && 0 <= n.event.key.row && n.event.key.row <= 2) || \
