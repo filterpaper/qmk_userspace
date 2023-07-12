58435841d1dec78f9bca670961725929d17c5e7f
フィルターペーパー <76888457+filterpaper@users.noreply.github.com>
2023-07-12 13:29:19 +0800

    Simplify RGB matrix indicators

--- rgb/rgb-matrix.c
+++ rgb/rgb-matrix.c
@@ -36,11 +36,7 @@ static inline RGB hsv_to_rgb_glow(HSV hsv) {
 bool rgb_matrix_indicators_user(void) {
     if (host_keyboard_led_state().caps_lock) {
         RGB const rgb = hsv_to_rgb_glow((HSV){HSV_CAPS});
-        for (uint8_t i = 0; i < RGB_MATRIX_LED_COUNT; ++i) {
-            if (g_led_config.flags[i] & CAP_FLAG) {
-                rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
-            }
-        }
+        rgb_matrix_set_color_all(rgb.r, rgb.g, rgb.b);
     }
 #ifdef SWAP_HANDS_ENABLE
     if (is_swap_hands_on()) {
@@ -48,33 +44,21 @@ bool rgb_matrix_indicators_user(void) {
         rgb_matrix_set_color_all(rgb.r, rgb.g, rgb.b);
     }
 #endif
+#ifdef CONVERT_TO_KB2040
     if (get_mods()) {
         uint8_t const mods = get_mods();
-        // Scale hue to mod bits
-        HSV const hsv = {(mods >> 4 | mods) * 16, rgb_matrix_config.hsv.s, rgb_matrix_config.hsv.v};
-        RGB const rgb = hsv_to_rgb(hsv);
+        RGB const rgb = hsv_to_rgb((HSV){(mods >> 4 | mods) * 16, 255, rgb_matrix_config.hsv.v});
         for (uint8_t i = 0; i < RGB_MATRIX_LED_COUNT; ++i) {
-            // Mask left and right mods
             if (g_led_config.flags[i] & mods) {
                 rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
             }
         }
     }
+#endif
     if (get_highest_layer(layer_state) > CMK) {
         uint8_t const layer = get_highest_layer(layer_state);
-        // Scale hue to layer number
-        HSV const hsv = {layer * 48, rgb_matrix_config.hsv.s, rgb_matrix_config.hsv.v};
-        RGB const rgb = hsv_to_rgb(hsv);
-        for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
-            for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
-                uint8_t  const led = g_led_config.matrix_co[row][col];
-                uint16_t const key = keymap_key_to_keycode(layer, (keypos_t){col, row});
-                // Match only LEDs with configured keycodes
-                if (led != NO_LED && key > KC_TRNS) {
-                    rgb_matrix_set_color(led, rgb.r, rgb.g, rgb.b);
-                }
-            }
-        }
+        RGB const rgb = hsv_to_rgb((HSV){layer * 48, 255, rgb_matrix_config.hsv.v});
+        rgb_matrix_set_color_all(rgb.r, rgb.g, rgb.b);
     }
     return false;
 }
