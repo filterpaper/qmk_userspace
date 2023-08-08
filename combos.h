10f59b883792ef17d2f96dfe9af7de383d9d1507
フィルターペーパー <76888457+filterpaper@users.noreply.github.com>
2023-08-09 07:08:50 +0800

    Refactor combo preprocessors

--- combos.h
+++ combos.h
@@ -5,14 +5,14 @@
 An adaptation of Jane Bernhardt's Combos on Steroids (http://combos.gboards.ca/)
 These preprocessors will build combo source codes from the following macros:
 COMB(name, keycode_shortcut, combo_sequence...)
-SUBS(name, "string to send", combo_sequence...)
+SSTR(name, "string to send", combo_sequence...)
 ACTN(name, function_call(),  combo_sequence...)
 
 Use the COMB macro for simple keycode shortcuts
 Example: COMB(vol_up, KC_VOLU, KC_Y, KC_U).
 
-Use the SUBS macro for sending strings
-Example: SUBS(which, "which ", KC_W, KC_H).
+Use the SSTR macro for sending strings
+Example: SSTR(which, "which ", KC_W, KC_H).
 
 Use the ACTN macro for function callbacks
 Example: ACTN(rgb_tog, rgb_matrix_toggle(), KC_Z, KC_X)
@@ -29,35 +29,35 @@ Example: ACTN(rgb_tog, rgb_matrix_toggle(), KC_Z, KC_X)
 #define UNUSED(...)
 
 #define COMB C_ENUM
-#define SUBS C_ENUM
+#define SSTR C_ENUM
 #define ACTN C_ENUM
 enum combos {
 #   include COMBOS_DEF
 };
 
 #undef COMB
-#undef SUBS
+#undef SSTR
 #undef ACTN
 #define COMB C_DATA
-#define SUBS C_DATA
+#define SSTR C_DATA
 #define ACTN C_DATA
 #include COMBOS_DEF
 
 #undef COMB
-#undef SUBS
+#undef SSTR
 #undef ACTN
 #define COMB C_TYPE
-#define SUBS A_TYPE
+#define SSTR A_TYPE
 #define ACTN A_TYPE
 combo_t key_combos[] = {
 #   include COMBOS_DEF
 };
 
 #undef COMB
-#undef SUBS
+#undef SSTR
 #undef ACTN
 #define COMB UNUSED
-#define SUBS P_SSTR
+#define SSTR P_SSTR
 #define ACTN P_ACTN
 void process_combo_event(uint16_t combo_index, bool pressed) {
     switch (combo_index) {
