6446d3c1f9108f0fa8455cb491889e4d864d353b
フィルターペーパー <76888457+filterpaper@users.noreply.github.com>
2023-09-04 09:08:34 +0800

    Refactor hold tap code
    Re-balance tap and debounce intervals

--- filterpaper.h
+++ filterpaper.h
@@ -15,12 +15,11 @@ __attribute__((always_inline)) static inline bool process_tap_hold(uint16_t keyc
 #define GET_MT_MOD_BITS(kc) ((kc & 0x1000) ? ((kc >> 8) & 0x0f) << 4 : (kc >> 8) & 0x0f)
 
 // Tap-hold decision helper macros
-#define IS_TYPING()          (timer_elapsed_fast(tap_timer) < TAPPING_TERM * 2)
 #define IS_HOMEROW(r)        (r->event.key.row == 1 || r->event.key.row == 5)
 #define IS_MOD_TAP_SHIFT(kc) (QK_MOD_TAP_GET_MODS(kc) & MOD_LSFT)
 #define IS_MOD_TAP_CS(kc)    (QK_MOD_TAP_GET_MODS(kc) & (MOD_LCTL | MOD_LSFT))
 
-#define IS_UNILATERAL_TAP(r, n) ((r->event.key.col != n.event.key.col) && (    \
+#define IS_UNILATERAL_TAP(r, n) ( r->event.key.col != n.event.key.col &&    (  \
     (r->event.key.row == 1 && 0 <= n.event.key.row && n.event.key.row <= 2) || \
     (r->event.key.row == 5 && 4 <= n.event.key.row && n.event.key.row <= 6) ))
 
