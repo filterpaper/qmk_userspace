b74f03ac5819f086992d024731a41f01e446163a
フィルターペーパー <76888457+filterpaper@users.noreply.github.com>
2024-01-01 12:16:15 +0800

    Simplify instant tap conditions
    * Evaluate previous keycodes at key press
    * Evaluate only is_pressed buffer at key release
    * Simplify combo event detection

--- filterpaper.h
+++ filterpaper.h
@@ -16,10 +16,12 @@ __attribute__((always_inline)) static inline bool process_tap_hold(uint16_t keyc
 
 // Tap-hold decision helper macros
 #define IS_HOMEROW(r)        (r->event.key.row == 1 || r->event.key.row == 5)
-#define IS_MOD_TAP_SHIFT(kc) (QK_MOD_TAP_GET_MODS(kc) & MOD_LSFT)
+#define IS_MOD_TAP_SHIFT(kc) (QK_MOD_TAP_GET_MODS(kc) & (MOD_LSFT))
 #define IS_MOD_TAP_CS(kc)    (QK_MOD_TAP_GET_MODS(kc) & (MOD_LCTL | MOD_LSFT))
 #define IS_MOD_TAP_CAG(kc)   (QK_MOD_TAP_GET_MODS(kc) & (MOD_LCTL | MOD_LALT | MOD_LGUI))
-#define IS_LAYER_TAP(kc)     (IS_QK_LAYER_TAP(kc) && QK_LAYER_TAP_GET_LAYER(kc))
+#define IS_LAYER_TAP(kc)     (IS_QK_LAYER_TAP(kc) &&  QK_LAYER_TAP_GET_LAYER(kc))
+#define IS_CLIPBOARD(kc)     (IS_QK_LAYER_TAP(kc) && !QK_LAYER_TAP_GET_LAYER(kc))
+#define IS_TYPING()          (last_input_activity_elapsed() < QUICK_TAP_TERM)
 
 #define IS_UNILATERAL_TAP(r, n) ( r->event.key.col != n.event.key.col &&    (  \
     (r->event.key.row == 1 && 0 <= n.event.key.row && n.event.key.row <= 2) || \
