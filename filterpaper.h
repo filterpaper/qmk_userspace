2a41eb500505867c2f9a2bfda6c87118ba8dea34
フィルターペーパー <76888457+filterpaper@users.noreply.github.com>
2024-01-20 19:59:01 +0800

    Update tap hold decision functions
    * Simplify macros
    * Increase unilateral range
    * Exclude clipboard shortcuts from per key tapping term
    * Update readme file

--- filterpaper.h
+++ filterpaper.h
@@ -19,15 +19,14 @@ __attribute__((always_inline)) static inline bool process_tap_hold(uint16_t keyc
 #define IS_MOD_TAP_SHIFT(kc) (QK_MOD_TAP_GET_MODS(kc) & (MOD_LSFT))
 #define IS_MOD_TAP_CS(kc)    (QK_MOD_TAP_GET_MODS(kc) & (MOD_LCTL | MOD_LSFT))
 #define IS_MOD_TAP_CAG(kc)   (QK_MOD_TAP_GET_MODS(kc) & (MOD_LCTL | MOD_LALT | MOD_LGUI))
-#define IS_LAYER_TAP(kc)     (IS_QK_LAYER_TAP(kc) &&  QK_LAYER_TAP_GET_LAYER(kc))
-#define IS_CLIPBOARD(kc)     (IS_QK_LAYER_TAP(kc) && !QK_LAYER_TAP_GET_LAYER(kc))
+#define IS_LAYER_TAP(kc)     (IS_QK_LAYER_TAP(kc) && QK_LAYER_TAP_GET_LAYER(kc))
 #define IS_TYPING()          (last_input_activity_elapsed() < QUICK_TAP_TERM)
 
-#define IS_UNILATERAL_TAP(r, n) ( r->event.key.col != n.event.key.col &&    (  \
-    (r->event.key.row == 1 && 0 <= n.event.key.row && n.event.key.row <= 2) || \
-    (r->event.key.row == 5 && 4 <= n.event.key.row && n.event.key.row <= 6) ))
+#define IS_UNILATERAL(r, n) ( \
+    (0 <= r->event.key.row && r->event.key.row <= 2 && 0 <= n.event.key.row && n.event.key.row <= 2) || \
+    (4 <= r->event.key.row && r->event.key.row <= 6 && 4 <= n.event.key.row && n.event.key.row <= 6) )
 
-#define IS_BILATERAL_TAP(r, n) ( \
+#define IS_BILATERAL(r, n) ( \
     (r->event.key.row == 1 && 4 <= n.event.key.row && n.event.key.row <= 7) || \
     (r->event.key.row == 5 && 0 <= n.event.key.row && n.event.key.row <= 3) )
 
