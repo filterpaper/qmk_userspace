da023cdb12c94594b3f3e06a44c679ea280e892f
フィルターペーパー <76888457+filterpaper@users.noreply.github.com>
2024-01-30 16:43:34 +0800

    Add prior keycode range for tap-hold override
    * Limit tap-hold override to prior keycode range
    * Update feature code in readme

--- filterpaper.h
+++ filterpaper.h
@@ -11,9 +11,9 @@ __attribute__((flatten)) static inline bool process_caps_unlock(uint16_t keycode
 #endif
 __attribute__((always_inline)) static inline bool process_tap_hold(uint16_t keycode, keyrecord_t *record);
 
-// Return mod-tap modifiers in 8bit for MOD_MASK_* logical operations
+// Convert 5-bit packed mod-tap modifiers to 8-bit packed MOD_MASK modifiers
 #define GET_MT_MOD_BITS(kc) (((kc) & 0x1000) ? ((kc >> 8) & 0x0f) << 4 : (kc >> 8) & 0x0f)
-// Basic keycode filter
+// Basic keycode filter for tap-hold keys
 #define GET_TAP_KEYCODE(kc) ((kc) & 0xff)
 
 // Tap-hold decision helper macros
@@ -22,7 +22,8 @@ __attribute__((always_inline)) static inline bool process_tap_hold(uint16_t keyc
 #define IS_MOD_TAP_CS(kc)    (IS_QK_MOD_TAP(kc) && QK_MOD_TAP_GET_MODS(kc) & (MOD_LCTL | MOD_LSFT))
 #define IS_MOD_TAP_CAG(kc)   (IS_QK_MOD_TAP(kc) && QK_MOD_TAP_GET_MODS(kc) & (MOD_LCTL | MOD_LALT | MOD_LGUI))
 #define IS_LAYER_TAP(kc)     (IS_QK_LAYER_TAP(kc) && QK_LAYER_TAP_GET_LAYER(kc))
-#define IS_TYPING()          (last_input_activity_elapsed() < QUICK_TAP_TERM)
+#define IS_TEXT(kc)          (KC_A <= (uint8_t)kc && (uint8_t)kc <= KC_SLSH)
+#define IS_TYPING(kc)        (last_input_activity_elapsed() < QUICK_TAP_TERM && IS_TEXT(kc) && !IS_LAYER_TAP(kc))
 
 #define IS_UNILATERAL(r, n) ( \
     (0 <= r->event.key.row && r->event.key.row <= 2 && 0 <= n.event.key.row && n.event.key.row <= 2) || \
