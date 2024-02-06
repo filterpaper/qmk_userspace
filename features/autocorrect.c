b0047eea57271ee77c6e3f9827ec5961bb277af7
フィルターペーパー <76888457+filterpaper@users.noreply.github.com>
2024-02-06 15:36:59 +0800

    Refactor source files
    * Remove archive sources
    * Group features into a single folder
    * Refactor RGB matrix options

--- features/autocorrect.c
+++ features/autocorrect.c
@@ -4,8 +4,9 @@
 // Adapted from Pascal Getreuer's Autocorrection
 // https://getreuer.info/posts/keyboards/autocorrection
 
-
 #include QMK_KEYBOARD_H
+
+#include "autocorrect.h"
 #include <string.h>
 #ifdef __AVR__
 #   include "autocorrect_data_avr.h"
@@ -24,33 +25,27 @@ bool process_autocorrect(uint16_t keycode, keyrecord_t* record) {
     static uint8_t buffer_size = 0;
 
     // Reset with non-Shift modifiers or when feature is off.
-    if (get_mods() & ~MOD_MASK_SHIFT || autocorrect_on == false) {
+    if (get_mods() & ~MOD_MASK_SHIFT || !autocorrect_on) {
         buffer_size = 0;
         return true;
     }
 
-    // Handle modifiers and quantum keycodes.
-    switch (keycode) {
-        case KC_LSFT:
-        case KC_RSFT:
-        case KC_CAPS:
-            return true;
-        case QK_MOD_TAP ... QK_MOD_TAP_MAX:
-        case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
-            // Exclude hold keycode
-            if (record->tap.count == 0) return true;
+    if (IS_QK_MOD_TAP(keycode) || IS_QK_LAYER_TAP(keycode)) {
+        // Exclude hold keycode.
+        if (!record->tap.count) return true;
+        keycode &= 0xFF;
     }
 
     // Handle non-alpha keycodes.
-    if (!(KC_A <= (uint8_t)keycode && (uint8_t)keycode <= KC_Z)) {
-        if (KC_TAB <= (uint8_t)keycode && (uint8_t)keycode <= KC_SLASH) {
+    if (keycode > KC_Z) {
+        if (KC_TAB <= keycode && keycode <= KC_SLASH) {
             // Replace punctuation with Space as word boundary.
             keycode = KC_SPC;
-        } else if ((uint8_t)keycode == KC_ENT) {
+        } else if (keycode == KC_ENT) {
             // Reset buffer and replace Enter with Space as word boundary.
             buffer_size = 0;
             keycode     = KC_SPC;
-        } else if ((uint8_t)keycode == KC_BSPC && buffer_size > 0) {
+        } else if (keycode == KC_BSPC && buffer_size > 0) {
             // Subtract buffer for Backspace.
             --buffer_size;
             return true;
@@ -68,7 +63,7 @@ bool process_autocorrect(uint16_t keycode, keyrecord_t* record) {
     }
 
     // Append keycode to buffer.
-    typo_buffer[buffer_size++] = (uint8_t)keycode;
+    typo_buffer[buffer_size++] = keycode;
     // Return if buffer is smaller than the shortest word.
     if (buffer_size < DICTIONARY_MIN_LENGTH) return true;
 
@@ -78,11 +73,12 @@ bool process_autocorrect(uint16_t keycode, keyrecord_t* record) {
     for (int8_t i = buffer_size - 1; i >= 0; --i) {
         if (code & 64) {  // Check for match in node with multiple children.
             code &= 63;
-            for (; code != typo_buffer[i]; code = pgm_read_byte(dictionary + (state += 3))) {
-                if (!code) return true;
+            while (code != typo_buffer[i]) {
+                if (!code) return true; // Not found
+                code = pgm_read_byte(dictionary + (state += 3));
             }
             // Follow link to child node.
-            state = (pgm_read_byte(dictionary + state + 1) | pgm_read_byte(dictionary + state + 2) << 8);
+            state = pgm_read_byte(dictionary + state + 1) | (pgm_read_byte(dictionary + state + 2) << 8);
         // Otherwise check for match in node with a single child.
         } else if (code != typo_buffer[i]) {
             return true;
@@ -96,8 +92,8 @@ bool process_autocorrect(uint16_t keycode, keyrecord_t* record) {
         code = pgm_read_byte(dictionary + state);
 
         if (code & 128) {  // A typo was found! Apply correction.
-            uint8_t const backspaces = code & 63;
-            for (uint8_t i = 0; i < backspaces; ++i) tap_code(KC_BSPC);
+            const uint8_t backspaces = code & 63;
+            for (uint8_t j = 0; j < backspaces; ++j) tap_code(KC_BSPC);
             send_string_P((char const *)(dictionary + state + 1));
 
             if (keycode == KC_SPC) {
