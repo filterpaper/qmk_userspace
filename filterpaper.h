84f407edeeb4ec26e5824bf9359c0b34faad027d
フィルターペーパー <76888457+filterpaper@users.noreply.github.com>
2024-02-13 08:11:11 +0800

    Update tap hold code and macros
    * Add text keycode detection for tap hold override
    * Remove combo event evaluation on tap hold override
    * Refactor 5-bit packed mod-tap bit macro
    * Increase tapping and quick tap term

--- filterpaper.h
+++ filterpaper.h
@@ -10,17 +10,17 @@
 #endif
 
 // Convert 5-bit packed mod-tap modifiers to 8-bit packed MOD_MASK modifiers
-#define GET_MT_MOD_BITS(kc) (((kc) & 0x1000) ? ((kc >> 8) & 0x0f) << 4 : (kc >> 8) & 0x0f)
+#define MOD_TAP_GET_MOD_BITS(kc) (((kc) & 0x0f00) >> (((kc) & 0x1000) ? 4 : 8))
 // Basic keycode filter for tap-hold keys
 #define GET_TAP_KEYCODE(kc) ((kc) & 0xff)
 
 // Tap-hold decision helper macros
 #define IS_HOMEROW(r)        (r->event.key.row == 1 || r->event.key.row == 5)
-#define IS_MOD_TAP_SHIFT(kc) (IS_QK_MOD_TAP(kc) && QK_MOD_TAP_GET_MODS(kc) & (MOD_LSFT))
-#define IS_MOD_TAP_CS(kc)    (IS_QK_MOD_TAP(kc) && QK_MOD_TAP_GET_MODS(kc) & (MOD_LCTL | MOD_LSFT))
-#define IS_MOD_TAP_CAG(kc)   (IS_QK_MOD_TAP(kc) && QK_MOD_TAP_GET_MODS(kc) & (MOD_LCTL | MOD_LALT | MOD_LGUI))
+#define IS_MOD_TAP_SHIFT(kc) (IS_QK_MOD_TAP(kc) && (kc) & (QK_LSFT))
+#define IS_MOD_TAP_CS(kc)    (IS_QK_MOD_TAP(kc) && (kc) & (QK_LCTL | QK_LSFT))
+#define IS_MOD_TAP_CAG(kc)   (IS_QK_MOD_TAP(kc) && (kc) & (QK_LCTL | QK_LALT | QK_LGUI))
 #define IS_LAYER_TAP(kc)     (IS_QK_LAYER_TAP(kc) && QK_LAYER_TAP_GET_LAYER(kc))
-#define IS_TEXT(kc)          (KC_A <= (uint8_t)kc && (uint8_t)kc <= KC_SLSH)
+#define IS_TEXT(kc)          (KC_A <= (uint8_t)(kc) && (uint8_t)(kc) <= KC_SLSH)
 #define IS_TYPING(kc)        (last_input_activity_elapsed() < QUICK_TAP_TERM && IS_TEXT(kc) && !IS_LAYER_TAP(kc))
 
 #define IS_UNILATERAL(r, n) ( \
