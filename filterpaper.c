4de3b4533925655803f831f7298ab51c16896b53
フィルターペーパー <76888457+filterpaper@users.noreply.github.com>
2024-05-05 10:18:56 +0800

    Refactor source and build files
    * Remove redundant header file
    * Remove non-split side step
    * Use elite_pi target

--- filterpaper.c
+++ filterpaper.c
@@ -1,11 +1,40 @@
 // Copyright @filterpaper
 // SPDX-License-Identifier: GPL-2.0+
 
-#include "filterpaper.h"
+#include QMK_KEYBOARD_H
+#include "autocorrect.h"
+#ifdef COMBO_ENABLE
+#   include "combos.h"
+#endif
+
+// Convert 5-bit packed mod-tap modifiers to 8-bit packed MOD_MASK modifiers
+#define MOD_TAP_GET_MOD_BITS(kc) (((kc) & 0x0f00) >> (((kc) & 0x1000) ? 4 : 8))
+// Basic keycode filter for tap-hold keys
+#define GET_TAP_KEYCODE(kc) ((kc) & 0xff)
+
+// Tap-hold decision helper macros
+#define IS_HOMEROW(r)        (r->event.key.row == 1 || r->event.key.row == 5)
+#define IS_MOD_TAP_SHIFT(kc) (IS_QK_MOD_TAP(kc) && (kc) & (QK_LSFT))
+#define IS_MOD_TAP_CS(kc)    (IS_QK_MOD_TAP(kc) && (kc) & (QK_LCTL | QK_LSFT))
+#define IS_MOD_TAP_CAG(kc)   (IS_QK_MOD_TAP(kc) && (kc) & (QK_LCTL | QK_LALT | QK_LGUI))
+#define IS_LAYER_TAP(kc)     (IS_QK_LAYER_TAP(kc) && QK_LAYER_TAP_GET_LAYER(kc))
+
+#define IS_TYPING(kc) ( \
+    last_input_activity_elapsed() < INPUT_INTERVAL   && \
+    (KC_A <= (uint8_t)(kc) && (uint8_t)(kc) <= KC_0) && \
+    !IS_LAYER_TAP(kc) )
+
+#define IS_UNILATERAL(r, n) ( \
+    (0 <= r->event.key.row && r->event.key.row <= 2 && 0 <= n.event.key.row && n.event.key.row <= 2) || \
+    (4 <= r->event.key.row && r->event.key.row <= 6 && 4 <= n.event.key.row && n.event.key.row <= 6) )
+
+#define IS_BILATERAL(r, n) ( \
+    (r->event.key.row == 1 && 4 <= n.event.key.row && n.event.key.row <= 7) || \
+    (r->event.key.row == 5 && 0 <= n.event.key.row && n.event.key.row <= 3) )
+
 
 static uint16_t    next_keycode;
 static keyrecord_t next_record;
-
 
 bool pre_process_record_user(uint16_t keycode, keyrecord_t *record) {
     static uint16_t prev_keycode;
@@ -41,7 +70,7 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     // Activate layer with another key press
     if (IS_LAYER_TAP(keycode)) return true;
 
-    // Sent its tap keycode when non-Shift overlaps with another key on the same hand
+    // Send its tap keycode when non-Shift overlaps with another key on the same hand
     if (IS_UNILATERAL(record, next_record) && !IS_MOD_TAP_SHIFT(next_keycode)) {
         record->keycode = GET_TAP_KEYCODE(keycode);
         process_record(record);
@@ -102,7 +131,7 @@ static inline bool process_tap_hold(uint16_t keycode, keyrecord_t *record) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
-#ifdef COMBO_SHOULD_TRIGGER
+#if defined (COMBO_ENABLE) && defined (COMBO_SHOULD_TRIGGER)
         if (record->event.type != COMBO_EVENT) input_timer = timer_read_fast();
 #endif
         if (!process_autocorrect(keycode, record) || !process_caps_unlock(keycode, record)) return false;
@@ -119,8 +148,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 
 // Simplify unused magic config functions
+#ifndef MAGIC_ENABLE
 uint8_t mod_config(uint8_t mod) { return mod; }
 uint16_t keycode_config(uint16_t keycode) { return keycode; }
+#endif
 
 
 // Reduce matrix scanning delay
