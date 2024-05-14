1aa13b3c1ae6ad5a62d06904263eba85afd9c0bf
フィルターペーパー <76888457+filterpaper@users.noreply.github.com>
2024-05-14 19:02:06 +0800

    Refactor split build directive
    * Override TARGET variable in makefile
    * Use transient EEPROM driver for RP2040
    * Remove TARGET parameter from workflow steps

--- rules.mk
+++ rules.mk
@@ -5,14 +5,22 @@ GRAVE_ESC_ENABLE = no
 SPACE_CADET_ENABLE = no
 
 # Enable common features
-LTO_ENABLE = yes
 COMBO_ENABLE = yes
 SWAP_HANDS_ENABLE = yes
 
-MAKECMDGOALS = uf2-split-$(SPLIT)
 VPATH += $(USER_PATH)/features
 INTROSPECTION_KEYMAP_C = filterpaper.c
 SRC += autocorrect.c
+
+ifneq ($(strip $(CONVERT_TO)),)
+    EEPROM_DRIVER = transient
+    LTO_ENABLE = $(if $(findstring kb2040,$(CONVERT_TO)),no,yes)
+    override TARGET := $(subst /,_,$(KEYBOARD))_$(strip $(CONVERT_TO))
+    ifneq ($(filter left right, $(SPLIT)),)
+        MAKECMDGOALS = uf2-split-$(strip $(SPLIT))
+        override TARGET := $(TARGET)_$(strip $(SPLIT))
+    endif
+endif
 
 ifeq ($(strip $(RGB_MATRIX_ENABLE)), yes)
     RGB_MATRIX_CUSTOM_USER = yes
@@ -21,7 +29,7 @@ endif
 
 ifeq ($(strip $(OLED_ENABLE)), yes)
     ifneq ($(strip $(OLED)),)
-        OPT_DEFS += -D$(OLED)
+        OPT_DEFS += -D$(strip $(OLED))
         SRC += oled_icons.c oled_luna.c
     else
         SRC += oled_icons.c oled_bongocat.c
