fdb66eff2fec58ce970094fe3518293c21951057
フィルターペーパー <76888457+filterpaper@users.noreply.github.com>
2024-06-05 15:51:14 +0800

    Tap hold override code updates
    * Increment tap count when overriding hold tap
    * Add Space to previous keycode evaluation
    * Refactor get tap code parameter
    * Adjust tapping term

--- features/combos.h
+++ features/combos.h
@@ -69,7 +69,7 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
 static fast_timer_t input_timer;
 
 bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode, keyrecord_t *record) {
-    if (timer_elapsed_fast(input_timer) < TAPPING_TERM) return false;
+    if (timer_elapsed_fast(input_timer) < TAPPING_TERM * 2) return false;
     else if (combo_index >= tog_num) return true;
     return get_highest_layer(layer_state) <= CMK;
 }
