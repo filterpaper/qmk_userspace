b6e6d866e396374e3fa98424e28a80451ea7162d
フィルターペーパー <76888457+filterpaper@users.noreply.github.com>
2024-10-13 08:12:38 +0800

    RGB matrix updates and refactor
    * Update RGB matrix defaults
    * Refactor user indicator function
    * Decouple prng function from effect
    * Use updated rgb_t and hsv_t structs
    * Use LED limits for Candy Rain
    * Add Candy Pulse RGB matrix effect
    * Add Pixel Mosaic RGB matrix effect

--- features/rgb_matrix.c
+++ features/rgb_matrix.c
@@ -4,11 +4,10 @@
 #include QMK_KEYBOARD_H
 #include <lib/lib8tion/lib8tion.h>
 
-#define RGB_DPINK   115, 20, 45
-#define RGB_DTEAL   5, 35, 35
-#define RGB_FLUOR   75, 122, 22
-#define RGB_CAPS    RGB_DPINK
-#define HSV_SWAP    HSV_TEAL
+#define RGB_DTEAL 5, 35, 35
+#define RGB_FLUOR 75, 122, 22
+#define HSV_SWAP  HSV_TEAL
+#define HSV_CAPS  HSV_PINK
 
 #ifdef CONVERT_TO_KB2040
 // Map keys to KB2040 LEDs on each side
@@ -18,7 +17,7 @@ led_config_t g_led_config = { {
     { 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1 },
     { 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1 }
 }, {
-    {109, 48}, {115, 48}
+    {0, 48}, {224, 48}
 }, {
     0x0f, 0xf0 // Flags for masking mod bits
 } };
@@ -31,39 +30,38 @@ layer_state_t layer_state_set_user(layer_state_t const state) {
 }
 
 
-static RGB hsv_to_rgb_glow(HSV hsv) {
+static rgb_t hsv_to_rgb_glow(hsv_t hsv) {
     // Return glowing RGB values
-    hsv.v = scale8(abs8(sin8(scale16by8(g_rgb_timer, rgb_matrix_config.speed / 8)) - 128) * 2, hsv.v);
+    hsv.v = scale8(abs8(sin8(scale16by8(g_rgb_timer, rgb_matrix_config.speed / 4)) - 128) * 2, hsv.v);
     return hsv_to_rgb(hsv);
 }
 
 
-bool rgb_matrix_indicators_user(void) {
-    if (host_keyboard_led_state().caps_lock) {
-        rgb_matrix_set_color_all(RGB_CAPS);
-    }
-
-#ifdef SWAP_HANDS_ENABLE
-    if (is_swap_hands_on()) {
-        RGB const rgb = hsv_to_rgb_glow((HSV){HSV_SWAP});
-        rgb_matrix_set_color_all(rgb.r, rgb.g, rgb.b);
-    }
-#endif
-
-#ifdef CONVERT_TO_KB2040
-    if (get_mods()) {
-        uint8_t const mods = get_mods();
-        RGB const rgb = hsv_to_rgb((HSV){(mods >> 4 | mods) * 16, 255, rgb_matrix_config.hsv.v});
-        for (uint8_t i = 0; i < RGB_MATRIX_LED_COUNT; ++i) {
-            if (g_led_config.flags[i] & mods) rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
-        }
-    }
-#endif
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    rgb_t rgb = {};
 
     if (get_highest_layer(layer_state) > CMK) {
-        uint8_t const layer = get_highest_layer(layer_state);
-        RGB const rgb = hsv_to_rgb((HSV){(layer - 1) * 80, 255, rgb_matrix_config.hsv.v});
-        rgb_matrix_set_color_all(rgb.r, rgb.g, rgb.b);
+        rgb = hsv_to_rgb((hsv_t){(get_highest_layer(layer_state) - 1) * 85, 255, rgb_matrix_config.hsv.v});
+    }
+
+#ifdef CONVERT_TO_KB2040
+    else if (get_mods() & g_led_config.flags[led_min]) {
+        rgb = hsv_to_rgb((hsv_t){(get_mods() >> 4 | get_mods()) * 16, 255, rgb_matrix_config.hsv.v});
+    }
+#endif
+
+#ifdef SWAP_HANDS_ENABLE
+    else if (is_swap_hands_on()) {
+        rgb = hsv_to_rgb_glow((hsv_t){HSV_SWAP});
+    }
+#endif
+
+    else if (host_keyboard_led_state().caps_lock) {
+        rgb = hsv_to_rgb_glow((hsv_t){HSV_CAPS});
+    }
+
+    if (rgb.r || rgb.g || rgb.b) {
+        for (uint8_t i = led_min; i < led_max; ++i) rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
     }
 
     return false;
