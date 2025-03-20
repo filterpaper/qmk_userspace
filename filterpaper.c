d336e755c1d5f8a2a0ac5c474a210b5959a527da
フィルターペーパー <76888457+filterpaper@users.noreply.github.com>
2025-03-20 09:24:14 +0800

    Refactor contextual tap hold code
    * Refactor macros with performant bitwise operations
    * Use packed bit array and bitwise operations
    * Reduces buffer size from 255 to 32 bytes
    * Use struct for contextual input
    * Add macro for contextual struct
    * Update readme code example

--- filterpaper.c
+++ filterpaper.c
@@ -4,7 +4,8 @@
 #include QMK_KEYBOARD_H
 #include "autocorrect.h"
 
-// Convert 5-bit to 8-bit packed modifiers
+// Convert 5-bit to 8-bit packed modifiers by extracting the
+// upper bit and shift by 4 if bit 12 is set, otherwise by 8
 #define MOD_TAP_GET_MOD_BITS(k) (((k) & 0x0f00) >> (((k) & 0x1000) ? 4 : 8))
 // Basic keycode filter for tap-hold keys
 #define GET_TAP_KEYCODE(k) ((k) & 0xff)
@@ -15,40 +16,58 @@
 #define IS_MOD_TAP_SHIFT(k) (IS_QK_MOD_TAP(k) && (k) & QK_LSFT)
 #define IS_MOD_TAP_CAG(k) (IS_QK_MOD_TAP(k) && (k) & (QK_LCTL|QK_LALT|QK_LGUI))
 
-#define IS_HOMEROW(r) (r->event.key.row == 1 || r->event.key.row == 5)
+#define HOMEROW_MASK ((1U << 1) | (1U << 5)) // Bitmasks for rows 1 and 5
+#define IS_HOMEROW(r) (HOMEROW_MASK & (1U << (r)->event.key.row))
 #define IS_HOMEROW_SHIFT(k, r) (IS_HOMEROW(r) && IS_MOD_TAP_SHIFT(k))
 #define IS_HOMEROW_CAG(k, r) (IS_HOMEROW(r) && IS_MOD_TAP_CAG(k))
 
 #define IS_TYPING(k) ( \
-    ((uint8_t)(k) <= KC_Z || IS_SHORTCUT(k)) && \
+    (GET_TAP_KEYCODE(k) <= KC_Z || IS_SHORTCUT(k)) && \
     (last_input_activity_elapsed() < INPUT_IDLE_MS))
 
-#define IS_UNILATERAL(r, n) ( \
-    (r->event.key.row == 1 && 0 <= n.event.key.row && n.event.key.row <= 2) || \
-    (r->event.key.row == 5 && 4 <= n.event.key.row && n.event.key.row <= 6) )
+// Same-hand key press detection by comparing two key events using row bitmasks
+// Row 1 matches rows 0,1,2 (bitmask 0x07) and row 5 matches rows 4,5,6 (bitmask 0x70)
+#define UNILATERAL_MASK(h) ((h) == 1 ? 0x07 : ((h) == 5 ? 0x70 : 0x00))
+#define IS_UNILATERAL(r, c) (UNILATERAL_MASK((r)->event.key.row) & (1U << (c).event.key.row))
+#define IS_UNILATERAL_AND_NOT_SHIFT(r, c) (IS_UNILATERAL((r), (c).record) && !IS_MOD_TAP_SHIFT((c).keycode))
 
+// Contextual input storage
+static struct {
+    uint_fast16_t keycode;
+    keyrecord_t   record;
+} context;
 
-static uint16_t    inter_keycode;
-static keyrecord_t inter_record;
 
 bool pre_process_record_user(uint16_t keycode, keyrecord_t *record) {
-    static bool     is_pressed[UINT8_MAX];
-    const  uint16_t tap_keycode = GET_TAP_KEYCODE(keycode);
+    // Bit array for tracking pressed state of tap keycodes (0-255)
+    static uint32_t is_pressed[(UINT8_MAX + 31) / 32];
 
-    if (record->event.pressed) {
-        // Press the tap keycode if the tap-hold key follows a previous key swiftly
-        if ((IS_HOMEROW_CAG(keycode, record) || IS_SHORTCUT(keycode)) && IS_TYPING(inter_keycode)) {
-            is_pressed[tap_keycode] = true;
-            record->keycode         = tap_keycode;
+    if (IS_HOMEROW_CAG(keycode, record) || IS_SHORTCUT(keycode)) {
+        // Variables to manage the pressed state array
+        const uint_fast8_t  tap_keycode = GET_TAP_KEYCODE(keycode);
+        const uint_fast8_t  tap_index   = tap_keycode / 32;
+        const uint_fast32_t tap_bitmask = 1U << (tap_keycode % 32);
+
+        if (record->event.pressed) {
+            // Press the tap keycode when it follows the previous key swiftly
+            if (IS_TYPING(context.keycode)) {
+                is_pressed[tap_index] |= tap_bitmask;
+                record->keycode = tap_keycode;
+            }
+        } else {
+            // Release the tap keycode if pressed
+            if (is_pressed[tap_index] & tap_bitmask) {
+                is_pressed[tap_index] &= ~tap_bitmask;
+                record->keycode = tap_keycode;
+            }
         }
-        // Cache incoming input for in-progress and subsequent tap-hold decisions
-        inter_keycode = keycode;
-        inter_record  = *record;
     }
-    else if (is_pressed[tap_keycode]) {
-        // Release the pressed tap keycode
-        is_pressed[tap_keycode] = false;
-        record->keycode         = tap_keycode;
+
+    // Store the intermediate keycode and its associated
+    // key event record for contextual processing
+    if (record->event.pressed) {
+        context.keycode = keycode;
+        context.record  = *record;
     }
 
     return true;
@@ -59,14 +78,14 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     // If the tap-hold key overlaps with another non-Shift key on the same
     // hand or if the key is a shortcut overlapping with any other key,
     // clear its interrupted state and process the tap-hold key as a tap
-    if ((IS_UNILATERAL(record, inter_record) && !IS_MOD_TAP_SHIFT(inter_keycode)) || IS_SHORTCUT(keycode)) {
+    if (IS_UNILATERAL_AND_NOT_SHIFT(record, context) || IS_SHORTCUT(keycode)) {
         record->tap.interrupted = false;
         record->tap.count++;
         process_record(record);
         return false;
     }
     // Activate layer hold with another key press
-    else return IS_LAYER_TAP(keycode);
+    return IS_LAYER_TAP(keycode);
 }
 
 
