83720aaf1ead0430d8d463d0af50a4f7e0d2b023
フィルターペーパー <76888457+filterpaper@users.noreply.github.com>
2025-07-29 19:28:05 +0800

    Improve tapped state handling for contextual input
    * Refactor tap-hold key event preprocessing
    * Use simple boolean tapped state array for RP2040
    * Track all tapped state to prevent of lost key-up events
    * Refactor macros with multi-line and alpha row match
    * Refactor caps unlock and shortcut processing code
    * Update tap-hold intervals
    * Remove magic configuration functions

--- filterpaper.c
+++ filterpaper.c
@@ -6,156 +6,123 @@
 
 // Convert 5-bit to 8-bit packed modifiers by extracting the
 // upper bit and shift by 4 if bit 12 is set, otherwise by 8
-#define MOD_TAP_GET_MOD_BITS(k) (((k) & 0x0f00) >> (((k) & 0x1000) ? 4 : 8))
-// Basic keycode filter for tap-hold keys
-#define GET_TAP_KEYCODE(k) ((k) & 0xff)
+#define MOD_TAP_GET_MOD_BITS(k) (((k) & 0x0F00) >> (((k) & 0x1000) ? 4 : 8))
+#define GET_TAP_KEYCODE(k) ((k) & 0xFF)
 
-// Tap-hold decision helper macros
-#define IS_LAYER_TAP(k) (IS_QK_LAYER_TAP(k) && QK_LAYER_TAP_GET_LAYER(k))
-#define IS_SHORTCUT(k) (IS_QK_LAYER_TAP(k) && !QK_LAYER_TAP_GET_LAYER(k))
-#define IS_MOD_TAP_SHIFT(k) (IS_QK_MOD_TAP(k) && (k) & QK_LSFT)
-#define IS_MOD_TAP_CAG(k) (IS_QK_MOD_TAP(k) && (k) & (QK_LCTL|QK_LALT|QK_LGUI))
+#define IS_TAP_HOLD(k) (IS_QK_MOD_TAP((k)) || IS_QK_LAYER_TAP((k)))
+#define IS_SHIFT_TAP(k) (((k) & QK_LSFT) && IS_QK_MOD_TAP((k)))
+// Alpha row key detection using bitmask 0x77 for rows 0-2 and 4-6
+#define IS_ALPHA_ROW(r) ((1U << (r)->event.key.row) & 0x77)
 
-#define HOMEROW_MASK ((1U << 1) | (1U << 5)) // Bitmasks for rows 1 and 5
-#define IS_HOMEROW(r) (HOMEROW_MASK & (1U << (r)->event.key.row))
-#define IS_HOMEROW_SHIFT(k, r) (IS_HOMEROW(r) && IS_MOD_TAP_SHIFT(k))
-#define IS_HOMEROW_CAG(k, r) (IS_HOMEROW(r) && IS_MOD_TAP_CAG(k))
+// Checks for a non-Shift tap-hold key pressed in rapid succession
+// after a letter key (A–Z) within a short interval
+#define IS_QUICK_SUCCESSION_INPUT(k, r, ctx) (                    \
+    IS_TAP_HOLD((k)) && !IS_SHIFT_TAP((k)) && IS_ALPHA_ROW((r))   \
+    && GET_TAP_KEYCODE((ctx).keycode) <= KC_Z                     \
+    && last_matrix_activity_elapsed() <= QUICK_TAP_TERM           \
+)
 
-#define IS_TYPING(k) ( \
-    (GET_TAP_KEYCODE(k) <= KC_Z || IS_SHORTCUT(k)) && \
-    (last_input_activity_elapsed() < INPUT_IDLE_MS))
+// Same-hand key press detection by comparing two key events using bitmasks
+// where row 1 matches rows 0-2 (0x07) and row 5 matches rows 4-6 (0x70)
+#define LATERAL_MASK(n) ((n) == 1 ? 0x07 : ((n) == 5 ? 0x70 : 0x00))
+#define IS_UNILATERAL(r, c) (LATERAL_MASK((r)->event.key.row) & (1U << (c).event.key.row))
 
-// Same-hand key press detection by comparing two key events using row bitmasks
-// Row 1 matches rows 0,1,2 (bitmask 0x07) and row 5 matches rows 4,5,6 (bitmask 0x70)
-#define UNILATERAL_MASK(h) ((h) == 1 ? 0x07 : ((h) == 5 ? 0x70 : 0x00))
-#define IS_UNILATERAL(r, c) (UNILATERAL_MASK((r)->event.key.row) & (1U << (c).event.key.row))
-#define IS_UNILATERAL_AND_NOT_SHIFT(r, c) (IS_UNILATERAL((r), (c).record) && !IS_MOD_TAP_SHIFT((c).keycode))
+// Checks for a home row key overlapping another non-Shift key on the same
+// side of the keyboard, or a shortcut tap-hold key overlapping with any key
+#define IS_CHORDAL_TAP_INPUT(k, r, ctx) (                                \
+    (IS_UNILATERAL((r), (ctx).record) && !IS_SHIFT_TAP((ctx).keycode))   \
+    || (IS_QK_LAYER_TAP((k)) && QK_LAYER_TAP_GET_LAYER((k)) == 0)        \
+)
 
-// Contextual input storage
+
+// Tap keycode pressed state tracker
+static bool tapped[UINT8_MAX + 1];
+
+// Contextual input cache
 static struct {
-    uint_fast16_t keycode;
-    keyrecord_t   record;
+    uint16_t    keycode;
+    keyrecord_t record;
 } context;
 
 
 bool pre_process_record_user(uint16_t keycode, keyrecord_t *record) {
-    // Bit array for tracking pressed state of tap keycodes (0-255)
-    static uint32_t is_pressed[(UINT8_MAX + 31) / 32];
-
-    if (IS_HOMEROW_CAG(keycode, record) || IS_SHORTCUT(keycode)) {
-        // Variables to manage the pressed state array
-        const uint_fast8_t  tap_keycode = GET_TAP_KEYCODE(keycode);
-        const uint_fast8_t  tap_index   = tap_keycode / 32;
-        const uint_fast32_t tap_bitmask = 1U << (tap_keycode % 32);
-
-        if (record->event.pressed) {
-            // Press the tap keycode when it follows the previous key swiftly
-            if (IS_TYPING(context.keycode)) {
-                is_pressed[tap_index] |= tap_bitmask;
-                record->keycode = tap_keycode;
-            }
-        } else {
-            // Release the tap keycode if pressed
-            if (is_pressed[tap_index] & tap_bitmask) {
-                is_pressed[tap_index] &= ~tap_bitmask;
-                record->keycode = tap_keycode;
-            }
-        }
-    }
-
-    // Store the intermediate keycode and its associated
-    // key event record for contextual processing
     if (record->event.pressed) {
+        // Flag key as tapped and update the key record with its tap keycode
+        if (IS_QUICK_SUCCESSION_INPUT(keycode, record, context)) {
+            uint8_t tap_keycode = GET_TAP_KEYCODE(keycode);
+            tapped[tap_keycode] = true;
+            record->keycode     = tap_keycode;
+        }
+        // Store the intermediate input for contextual processing
         context.keycode = keycode;
         context.record  = *record;
+    } else {
+        // Clear tapped flag and set tap count for release
+        uint8_t tap_keycode = GET_TAP_KEYCODE(keycode);
+        if (tapped[tap_keycode]) {
+            tapped[tap_keycode] = false;
+            record->tap.count++;
+        }
     }
-
     return true;
 }
 
 
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
-    // If the tap-hold key overlaps with another non-Shift key on the same
-    // hand or if the key is a shortcut overlapping with any other key,
-    // clear its interrupted state and process the tap-hold key as a tap
-    if (IS_UNILATERAL_AND_NOT_SHIFT(record, context) || IS_SHORTCUT(keycode)) {
+    if (IS_CHORDAL_TAP_INPUT(keycode, record, context)) {
+        // Flag key as tapped if next keycode is a tap-hold key
+        if (IS_TAP_HOLD(context.keycode)) {
+            tapped[GET_TAP_KEYCODE(keycode)] = true;
+        }
         record->tap.interrupted = false;
         record->tap.count++;
-        process_record(record);
-        return false;
+        return true;
     }
     // Activate layer hold with another key press
-    return IS_LAYER_TAP(keycode);
-}
-
-
-bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
-    // Register Shift with a nested key press
-    return IS_HOMEROW_SHIFT(keycode, record);
+    return IS_QK_LAYER_TAP(keycode) && QK_LAYER_TAP_GET_LAYER(keycode);
 }
 
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
-    // Shorten interval for Shift
-    return IS_HOMEROW_SHIFT(keycode, record) ? SHIFT_TAP_TERM : TAPPING_TERM;
+    return IS_SHIFT_TAP(keycode) && IS_ALPHA_ROW(record) ? SHIFT_TAP_TERM : TAPPING_TERM;
 }
 
 
-// Turn off caps lock at word boundary
-static inline void process_caps_unlock(uint16_t keycode, keyrecord_t *record) {
+static inline bool process_caps_word(uint16_t keycode, keyrecord_t *record) {
+    if (!host_keyboard_led_state().caps_lock) return true;
     // Get tap keycode from tap-hold keys
-    if (IS_QK_MOD_TAP(keycode) || IS_QK_LAYER_TAP(keycode)) {
-        if (record->tap.count == 0) return;
+    if (IS_TAP_HOLD(keycode)) {
+        if (record->tap.count == 0) return true;
         keycode = GET_TAP_KEYCODE(keycode);
     }
-
-    switch (keycode) {
-        case KC_A ... KC_0:
-        case KC_BSPC:
-        case KC_MINS:
-        case KC_UNDS:
-        case KC_CAPS:
-            // Retain caps lock if there are no active non-Shift modifiers
-            if ((get_mods() & ~MOD_MASK_SHIFT) == 0) return;
-        default:
-            // Everything else is a word boundary
-            tap_code(KC_CAPS);
+    // Letter and word-related keys that retain caps lock
+    bool is_word_key = keycode <= KC_0
+        || keycode == KC_BSPC
+        || keycode == KC_MINS
+        || keycode == KC_UNDS
+        || keycode == KC_CAPS;
+    // Toggle caps lock at a word boundary if the key is not
+    // a word key or if any non-Shift modifiers are active
+    if (!is_word_key || (get_mods() & ~MOD_MASK_SHIFT)) {
+        tap_code(KC_CAPS);
     }
-}
-
-
-// Send custom hold keycode
-static inline bool process_tap_hold(uint16_t keycode, keyrecord_t *record) {
-    if (record->tap.count) return true;
-    tap_code16(keycode);
-    return false;
-}
-
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) {
-        if (!process_autocorrect(keycode, record)) return false;
-        if (host_keyboard_led_state().caps_lock) process_caps_unlock(keycode, record);
-
-        // Clipboard shortcuts
-        if      (keycode == TH_M)    return process_tap_hold(Z_PST, record);
-        else if (keycode == TH_COMM) return process_tap_hold(Z_CPY, record);
-        else if (keycode == TH_DOT)  return process_tap_hold(Z_CUT, record);
-        else if (keycode == TH_SLSH) return process_tap_hold(Z_UND, record);
-    }
-
     return true;
 }
 
 
-// Simplify unused magic config functions
-#ifndef MAGIC_ENABLE
-uint8_t mod_config(uint8_t mod) { return mod; }
-uint16_t keycode_config(uint16_t keycode) { return keycode; }
-#endif
+// Send shortcut keycode for held key
+#define PROCESS_SHORTCUT(k, r) ((r)->tap.count ? true : (tap_code16((k)), false))
 
-
-// Reduce matrix scanning delay
-#ifndef DIRECT_PINS
-void matrix_io_delay(void) { __asm__ volatile("nop\nnop\nnop\n"); }
-#endif
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        if (!process_autocorrect(keycode, record) || !process_caps_word(keycode, record)) {
+            return false;
+        }
+        // Handle clipboard tap-hold shortcuts
+        if      (keycode == TH_M)    return PROCESS_SHORTCUT(Z_PST, record);
+        else if (keycode == TH_COMM) return PROCESS_SHORTCUT(Z_CPY, record);
+        else if (keycode == TH_DOT)  return PROCESS_SHORTCUT(Z_CUT, record);
+        else if (keycode == TH_SLSH) return PROCESS_SHORTCUT(Z_UND, record);
+    }
+    return true;
+}
